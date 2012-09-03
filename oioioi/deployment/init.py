0ea8325b844a8836d4301552683ba05d9cc132f5
Szymon Acedański <accek@mimuw.edu.pl>
2012-09-03 14:20:03 +0200

    deployed manage.py now sets PYTHONPATH
    Otherwise the development server would fail, as django-staticfiles-lessc
    could not find settings. Same for wsgi.py.

---
+++ oioioi/deployment/init.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+def init_env(settings_dir):
+    sys.path.insert(0, settings_dir)
+    if os.environ.get('PYTHONPATH'):
+        os.environ['PYTHONPATH'] = settings_dir + ':' \
+            + os.environ['PYTHONPATH']
+    else:
+        os.environ['PYTHONPATH'] = settings_dir
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
