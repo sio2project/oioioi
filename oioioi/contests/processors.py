e252bdbc9d05198ae849415285dde785403f239e
Szymon Acedański <accek@mimuw.edu.pl>
2012-09-09 03:09:39 +0200

    extract recent_contests so that is can be used by others

--- oioioi/contests/processors.py
+++ oioioi/contests/processors.py
@@ -17,14 +17,17 @@ def register_current_contest(request):
     else:
         return {}
 
+def recent_contests(request):
+    ids = request.session.get('recent_contests', [])
+    mapping = Contest.objects.in_bulk(ids)
+    return filter(lambda c: c is not None and c != request.contest,
+            [mapping[id] for id in ids])
+
 def register_recent_contests(request):
     if not hasattr(request, 'contest') or not hasattr(request, 'session'):
         return {}
     def generator():
-        ids = request.session.get('recent_contests', [])
-        mapping = Contest.objects.in_bulk(ids)
-        return filter(lambda c: c is not None and c != request.contest,
-                [mapping[id] for id in ids])
+        return recent_contests(request)
     return {'recent_contests': lazy(generator, list)()}
 
 def register_only_default_contest(request):
