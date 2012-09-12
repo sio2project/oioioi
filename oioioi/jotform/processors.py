94acde9289acb15192ba5391f7b1949c48142e4c
Szymon Acedański <accek@mimuw.edu.pl>
2012-09-12 16:42:30 +0200

    host jotform.js locally and load it asynchronously
    Change-Id: I03b0d2f496165c6a9e802f84b479b29b0e8c9239

--- oioioi/jotform/processors.py
+++ oioioi/jotform/processors.py
@@ -1,11 +1,14 @@
 from django.conf import settings
-from django.template import Context
+from django.utils.functional import lazy
+from django.template import RequestContext
 from django.template.loader import render_to_string
 
 def jotform(request):
     if settings.JOTFORM_ID:
-        return {'extra_head_jotform':
-                render_to_string('jotform/jotform_head.html',
-                    Context({'jotform_id': settings.JOTFORM_ID}))}
+        def generator():
+            return render_to_string('jotform/jotform_head.html',
+                context_instance=RequestContext(request,
+                    {'jotform_id': settings.JOTFORM_ID}))
+        return {'extra_head_jotform': lazy(generator, unicode)()}
     else:
         return {}
