3d2a6f54db7b810c286416de9e7faeb5c3c09aab
Szymon Acedański <accek@mimuw.edu.pl>
2012-09-21 19:35:21 +0200

    automatically use urlpatterns from oioioi.* modules
    Change-Id: I9efc6dd938af87d88c623df65009a7ffead1bdcb

--- oioioi/urls.py
+++ oioioi/urls.py
@@ -1,13 +1,8 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.utils.importlib import import_module
 from django.contrib import admin as django_admin
 import oioioi.base.registration_backend
-import oioioi.contests.urls
-import oioioi.problems.urls
-import oioioi.programs.urls
-import oioioi.messages.urls
-import oioioi.rankings.urls
-import oioioi.dashboard.urls
 from oioioi.base import admin
 
 django_admin.autodiscover()
@@ -22,19 +17,16 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^grappelli/', include('grappelli.urls')),
 
-    url(r'^clock/', include('oioioi.clock.urls')),
     url(r'^file/(?P<filename>.*)$',
         'oioioi.filetracker.views.raw_file_view'),
 )
 
 urlpatterns += oioioi.base.registration_backend.urlpatterns
-urlpatterns += oioioi.contests.urls.urlpatterns
-urlpatterns += oioioi.problems.urls.urlpatterns
-urlpatterns += oioioi.programs.urls.urlpatterns
-urlpatterns += oioioi.messages.urls.urlpatterns
-urlpatterns += oioioi.rankings.urls.urlpatterns
-urlpatterns += oioioi.dashboard.urls.urlpatterns
 
-if 'oioioi.teachers' in settings.INSTALLED_APPS:
-    import oioioi.teachers.urls
-    urlpatterns += oioioi.teachers.urls.urlpatterns
+for app in settings.INSTALLED_APPS:
+    if app.startswith('oioioi.'):
+        try:
+            urls_module = import_module(app + '.urls')
+            urlpatterns += getattr(urls_module, 'urlpatterns')
+        except ImportError:
+            pass
