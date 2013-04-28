904254f484ba440c469c8161496f2724c7dad8ab
Maciej Matraszek <matraszek.maciej@gmail.com>
2013-04-28 19:23:47 +0200

    (no-ticket) Fix CSRF issues in teacher's app.
    Change-Id: Ie5cccc91c573161258810a8f9a20f8655b3e0fe0

---
+++ oioioi/base/utils/confirmation.py
@@ -0,0 +1,21 @@
+from django.template.response import TemplateResponse
+
+
+def confirmation_view(request,
+        template='simple-centered-confirmation.html', context=None):
+    """Renders simple confirmation boxes.
+
+       This function returns boolean if user has already confirmed/canceled
+       action and :class:`~django.template.response.TemplateResponse`
+       otherwise.
+    """
+    if context is None:
+        context = {}
+
+    if request.method == 'POST':
+        if 'confirmation' in request.POST:
+            return True
+        else:
+            return False
+
+    return TemplateResponse(request, template, context)
--- oioioi/teachers/views.py
+++ oioioi/teachers/views.py
@@ -8,8 +8,10 @@ from django.core.exceptions import SuspiciousOperation, PermissionDenied
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.views.decorators.http import require_POST
 
 from oioioi.base.menu import account_menu_registry
+from oioioi.base.utils.confirmation import confirmation_view
 from oioioi.contests.menu import contest_admin_menu_registry
 from oioioi.contests.models import Contest
 from oioioi.participants.models import Participant
@@ -91,14 +93,20 @@ def accept_teacher_view(request, user_id):
     if teacher.is_active:
         messages.info(request, _("User already accepted."))
     else:
-        teacher.is_active = True
-        teacher.save()
-        send_acceptance_email(request, teacher)
-        messages.success(request, _("Successfully accepted and emailed the "
-            "new teacher."))
+        choice = confirmation_view(request, 'teachers/confirm_add_teacher.html',
+                {'teacher': teacher})
+        if not isinstance(choice, bool):
+            return choice
+        if choice:
+            teacher.is_active = True
+            teacher.save()
+            send_acceptance_email(request, teacher)
+            messages.success(request, _("Successfully accepted and emailed the "
+                "new teacher."))
     return redirect('oioioiadmin:teachers_teacher_changelist')
 
 @enforce_condition(is_contest_admin)
+@enforce_condition(is_teachers_contest)
 def pupils_view(request, contest_id):
     teachers = User.objects \
             .filter(teacher__contestteacher__contest=request.contest)
@@ -120,30 +128,29 @@ def pupils_view(request, contest_id):
             })
 
 @enforce_condition(not_anonymous)
+@enforce_condition(is_teachers_contest)
 def activate_pupil_view(request, contest_id, key):
     registration_config = get_object_or_404(RegistrationConfig,
             contest=request.contest)
     key_ok = registration_config.key == key
     if key_ok and registration_config.is_active:
         is_teacher = request.user.has_perm('teachers.teacher')
-        if not is_teacher:
-            register_as = 'pupil'
-        elif 'register_as' in request.REQUEST:
-            register_as = request.REQUEST['register_as']
+        if not request.method == 'POST' or 'register_as' not in request.POST:
+            return TemplateResponse(request, 'teachers/confirm_join.html',
+                {'key': key, 'is_teacher': is_teacher})
         else:
-            return TemplateResponse(request, 'teachers/activation_type.html',
-                    {'key': key})
-        if register_as == 'pupil':
-            Participant.objects.get_or_create(contest=request.contest,
-                    user=request.user)
-        elif register_as == 'teacher':
-            teacher_obj = get_object_or_404(Teacher, user=request.user)
-            ContestTeacher.objects.get_or_create(contest=request.contest,
-                    teacher=teacher_obj)
-        else:
-            raise SuspiciousOperation
-        messages.info(request, _("Activation successful."))
-        return redirect('default_contest_view', contest_id=contest_id)
+            register_as = request.POST['register_as']
+            if register_as == 'pupil':
+                Participant.objects.get_or_create(contest=request.contest,
+                        user=request.user)
+            elif is_teacher and register_as == 'teacher':
+                teacher_obj = get_object_or_404(Teacher, user=request.user)
+                ContestTeacher.objects.get_or_create(contest=request.contest,
+                        teacher=teacher_obj)
+            else:
+                raise SuspiciousOperation
+            messages.info(request, _("Activation successful."))
+            return redirect('default_contest_view', contest_id=contest_id)
     return TemplateResponse(request, 'teachers/activation_error.html', {
                 'key_ok': key_ok,
                 'registration_active': registration_config.is_active,
@@ -153,7 +160,9 @@ def redirect_to_pupils(request):
     return redirect(reverse(pupils_view, kwargs={'contest_id':
         request.contest.id}))
 
+@require_POST
 @enforce_condition(is_contest_admin)
+@enforce_condition(is_teachers_contest)
 def set_registration_view(request, contest_id, value):
     registration_config = get_object_or_404(RegistrationConfig,
             contest=request.contest)
@@ -161,7 +170,9 @@ def set_registration_view(request, contest_id, value):
     registration_config.save()
     return redirect_to_pupils(request)
 
+@require_POST
 @enforce_condition(is_contest_admin)
+@enforce_condition(is_teachers_contest)
 def regenerate_key_view(request, contest_id):
     registration_config = get_object_or_404(RegistrationConfig,
             contest=request.contest)
@@ -169,7 +180,9 @@ def regenerate_key_view(request, contest_id):
     registration_config.save()
     return redirect_to_pupils(request)
 
+@require_POST
 @enforce_condition(is_contest_admin)
+@enforce_condition(is_teachers_contest)
 def delete_pupils_view(request, contest_id):
     ContestTeacher.objects.filter(contest=request.contest,
             teacher__user_id__in=request.POST.getlist('teacher')).delete()
@@ -177,7 +190,9 @@ def delete_pupils_view(request, contest_id):
             user_id__in=request.POST.getlist('pupil')).delete()
     return redirect_to_pupils(request)
 
+@require_POST
 @enforce_condition(is_contest_admin)
+@enforce_condition(is_teachers_contest)
 def bulk_add_pupils_view(request, contest_id, other_contest_id):
     other_contest = get_object_or_404(Contest, id=other_contest_id)
     if not request.user.has_perm('contests.contest_admin', other_contest):
