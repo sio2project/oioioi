0c82b3a014f81210aa095c193c28f9f0b9b5f432
Maciej Dębski <md319428@students.mimuw.edu.pl>
2013-05-16 08:59:22 +0200

    (no-ticket) wsgi: seems that 5 lines were needed...
    Change-Id: Ibab7afd79060fa9eac2e124b1d99c9cd9fe19ac1

--- oioioi/deployment/init.py
+++ oioioi/deployment/init.py
@@ -3,4 +3,9 @@ import sys
 
 def init_env(settings_dir):
     sys.path.insert(0, settings_dir)
+    if os.environ.get('PYTHONPATH'):
+        os.environ['PYTHONPATH'] = settings_dir + ':' \
+            + os.environ['PYTHONPATH']
+    else:
+        os.environ['PYTHONPATH'] = settings_dir
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
