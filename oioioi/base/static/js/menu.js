f25ce22909e4f693e2bfd9e1f0b71dc87970da1a
Maciej Matraszek <matraszek.maciej@gmail.com>
2013-07-02 18:38:43 +0200

    (no-ticket) Fix TypeError in menu.js on sites without menu
    Change-Id: I480302ade32fd0b6ace44a17c2a38a146dc8f5be

--- oioioi/base/static/js/menu.js
+++ oioioi/base/static/js/menu.js
@@ -1,42 +1,45 @@
 $(window).load(function(){
-    var menu_ul = '#menu_div .nav-list';
-    var last_li = menu_ul + ' > li:nth-last-of-type(3)';
-    var submenu = menu_ul + ' .nav-dropdown-submenu';
-    var submenu_ul = submenu + ' > ul';
-    var submenu_first_li = submenu_ul + ' > li:first';
-    var submenu_divider = '#nav-dropdown-divider';
+    var menu_div = $('#menu_div');
+    var menu_ul = menu_div.find('.nav-list');
+    var last_li = function(){return menu_ul.children('li:nth-last-of-type(3)');};
+    var submenu = menu_ul.find('.nav-dropdown-submenu');
+    var submenu_ul = submenu.children('ul');
+    var submenu_first_li = function(){return submenu_ul.children('li:first');};
+    var submenu_divider = $('#nav-dropdown-divider');
     var footer = $('.body-with-menu footer');
 
 
     var hide_first = function() {
-        if ($(submenu_ul).is(':empty')) {
-            $(submenu_divider).show();
-            $(submenu).show();
+        if (submenu_ul.is(':empty')) {
+            submenu_divider.show();
+            submenu.show();
         }
 
-        $(last_li).prependTo(submenu_ul);
+        last_li().prependTo(submenu_ul);
     };
 
     var show_first = function() {
-        $(submenu_first_li).insertBefore(submenu_divider);
-        if ($(submenu_ul).is(':empty')) {
-            $(submenu_divider).hide();
-            $(submenu).hide();
+        submenu_first_li().insertBefore(submenu_divider);
+        if (submenu_ul.is(':empty')) {
+            submenu_divider.hide();
+            submenu.hide();
         }
     };
 
     var recalculate_menu = function() {
-        var ul = $(menu_ul);
-        var cut_y = footer.length ? footer.offset().top
-                : $('#menu_div').offset().top + $(window).height();
+        if (menu_div.length == 0)
+            return;
 
-        while ($(submenu_first_li).length == 1
-            && ul.offset().top + ul.height() < cut_y) {
+        var cut_y = footer.length ? footer.offset().top
+                : menu_div.offset().top + $(window).height();
+
+        while (submenu_first_li().length == 1
+            && menu_ul.offset().top + menu_ul.height() < cut_y) {
             show_first();
         }
 
-        while ($(last_li).length == 1
-            && ul.offset().top + ul.height() > cut_y) {
+        while (last_li().length == 1
+            && menu_ul.offset().top + menu_ul.height() > cut_y) {
             hide_first();
         }
     };
