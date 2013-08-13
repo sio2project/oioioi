aa2fb5bf73dab2eea3ffd92642b67570553410fb
Szymon Acedański <accek@mimuw.edu.pl>
2013-08-13 09:17:34 +0200

    (no ticket) Allow contest admins change test run configs and score revealing
    Change-Id: I933b1a47d86132395dab41a7659feb6fd32f18cb

--- oioioi/scoresreveal/admin.py
+++ oioioi/scoresreveal/admin.py
@@ -29,6 +29,15 @@ class ScoresRevealConfigInline(admin.TabularInline):
     can_delete = True
     extra = 0
 
+    def has_add_permission(self, request):
+        return True
+
+    def has_change_permission(self, request, obj=None):
+        return True
+
+    def has_delete_permission(self, request, obj=None):
+        return True
+
 
 class ScoresRevealProgrammingProblemAdminMixin(object):
     def __init__(self, *args, **kwargs):
