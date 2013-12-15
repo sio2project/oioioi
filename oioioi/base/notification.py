c3165889dd10e9b30d3255a4f07696198e7f3c3b
Marek Bardoński <bdfhjk@gmail.com>
2013-12-15 05:47:37 +0100

    SIO-1386 Find and upgrade the logging function, allowing to emit notifications
    Change-Id: I7150252cfc805675b576072c316f438a53d3e0d1

---
+++ oioioi/base/notification.py
@@ -0,0 +1,48 @@
+import logging
+
+
+class NotificationHandler(logging.StreamHandler):
+    """This handler will catch all logs and emit a notification
+       if a notification type is set in extra dictionary, in log record.
+
+       Example usage is in external documentation.
+    """
+    @staticmethod
+    def send_notification(user_id, notification_type, notification_message):
+        """This function will send a notification for a specified person.
+           It will be done by sending a message to RabbitMQ.
+        """
+        pass
+
+    @staticmethod
+    def register(notification_type, notification_function):
+        """Register a specific notification handler function for
+           specified type of notification, that will be executed
+           each time the log with this notification type will be
+           processed.
+        """
+        pass
+
+    def emit(self, record):
+        """This function is called each time a  message is logged.
+
+           In our design, it's role is to invoke a specific handler
+           for corresponding notification type, registered before
+           via :meth:`register` function by caller.
+
+           Specific notification handler should prepare translated
+           message string, split one event for particular users
+           and execute :meth:`send_notification` function for each user
+           who should be notified.
+        """
+
+        #
+        # Emit is called with a lock held, see
+        # http://docs.python.org/2/library/logging.html#logging.Handler.handle
+        #
+
+        if hasattr(record, 'notification'):
+            pass
+        else:
+            pass
+
