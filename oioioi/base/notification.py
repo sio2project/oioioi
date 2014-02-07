1c96e5520af0b9be6ddfb743a1e142b01bf6c0a2
Marek Bardoński <bdfhjk@gmail.com>
2014-02-07 22:41:12 +0100

    SIO-1406 / SIO-1392 Finish technical documentation
    Change-Id: I7f60011c379a376544a8454d7a1e46e52dca22f5

--- oioioi/base/notification.py
+++ oioioi/base/notification.py
@@ -4,11 +4,14 @@ from oioioi.base.utils.loaders import load_modules
 logger = logging.getLogger(__name__)
 
 class NotificationHandler(logging.StreamHandler):
-    """This handler will catch all logs and emit a notification
-       if a notification type is set in extra dictionary, in log record.
-
-       Example usage is in external documentation.
+    """This handler catches all logs and emits a notification
+       if a notification type is set in the extra dictionary,
+       in the log record.
     """
+
+    # Example usage may be found in external documentation.
+    # Link: http://oioioi.readthedocs.org/en/latest/sections/notifications.html
+
     loaded_notifications = False
 
     # This dictionary stores functions handling registered notifications
@@ -18,32 +21,35 @@ class NotificationHandler(logging.StreamHandler):
     @classmethod
     def send_notification(cls, user, notification_type,
             notification_message, notificaion_message_arguments):
-        """This function will send a notification for a specified person.
-           It will be done by sending a message to RabbitMQ.
+
+        """This function sends a notification to the specified user
+           by sending a message to RabbitMQ.
 
            :param user: User, to whom the notification will be sent.
 
-           :param notification_message: A message to show to notified user,
-               which will be translated by frontend to his language
+           :param notification_type: A string which describes the notification
+               type.
+
+           :param notification_message: A message to show to the notified user,
+               which will be translated by frontend to their language
                and interpolated using notification_message_arguments.
                Remember to mark this message to translate, passing it as
                argument to _() function, so that the message string will be
                caught to translate.
 
            :param notification_message_arguments: A map which contains
-               strings to interpolate notification_message, and special
-               optional parameter "link" -- absolute link
-               (starting with http://) to page related to
-               the notification, where user can check the details.
+               strings to interpolate notification_message, and a special
+               optional parameter "link" -- an absolute link
+               (starting with http://) to a page related to
+               the notification, where the user can check the details.
         """
         pass
 
     @classmethod
     def register_notification(cls, notification_type, notification_function):
-        """Register a specific notification handler function for
+        """Register a specific notification handler function for the
            specified type of notification, that will be executed
-           each time the log with this notification type will be
-           processed.
+           each time a log with this notification type is processed.
         """
         if notification_type in cls.notification_functions:
             logger.warning("Notification %s was registered twice",
