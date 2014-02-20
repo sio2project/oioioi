02a88246c73d28f85b85b4577c888083a30971bb
Marek Bardoński <bdfhjk@gmail.com>
2014-02-20 09:12:26 +0100

    SIO-1447 Add dashboard message
    Change-Id: I79cb88d55d4fa59f8225ae5a7ccabac1fa7a6b85

--- oioioi/dashboard/urls.py
+++ oioioi/dashboard/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, include, url
 
 contest_patterns = patterns('oioioi.dashboard.views',
     url(r'^dashboard/$', 'contest_dashboard_view', name='contest_dashboard'),
+    url(r'^dashboard-message/$', 'dashboard_message_edit_view',
+        name='dashboard_message_edit'),
 )
 
 urlpatterns = patterns('oioioi.dashboard.views',
