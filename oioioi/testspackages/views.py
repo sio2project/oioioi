d06b19b493da28d3686b5fd2b10df42c744de2d7
Michał Adamczyk <mr.adamczyk@gmail.com>
2014-04-01 17:43:22 +0200

    (no-ticket) tests packages visible only if problem instance visible
    Change-Id: I5f3a738d5047e39cd697d5e3f172449ca9d7288e

--- oioioi/testspackages/views.py
+++ oioioi/testspackages/views.py
@@ -8,29 +8,26 @@ from django.core.exceptions import PermissionDenied
 from django.utils.translation import ugettext_lazy as _
 
 from oioioi.base.menu import menu_registry
-from oioioi.base.permissions import not_anonymous, enforce_condition
-from oioioi.contests.models import ProblemInstance
+from oioioi.base.permissions import not_anonymous, enforce_condition, \
+        make_request_condition
+from oioioi.base.utils import request_cached
 from oioioi.contests.utils import can_enter_contest, contest_exists, \
-        is_contest_admin
+        is_contest_admin, visible_problem_instances
 from oioioi.filetracker.utils import stream_file
 from oioioi.testspackages.models import TestsPackage
-from oioioi.base.permissions import make_request_condition
-from oioioi.problems.models import Problem
+
+
+@request_cached
+def visible_tests_packages(request):
+    problems = [pi.problem for pi in visible_problem_instances(request)]
+    tests_packages = TestsPackage.objects.filter(problem__in=problems)
+    return [tp for tp in tests_packages
+            if tp.is_visible(request.timestamp) and tp.package is not None]
 
 
 @make_request_condition
 def is_any_tests_package_visible(request):
-    pis = ProblemInstance.objects.filter(contest=request.contest)
-    for pi in pis:
-        try:
-            problem = pi.problem
-        except Problem.DoesNotExist:
-            continue
-        tests_packages = TestsPackage.objects.filter(problem=problem)
-        for tp in tests_packages:
-            if tp.is_visible(request.timestamp) and tp.package is not None:
-                return True
-    return False
+    return bool(visible_tests_packages(request))
 
 
 @menu_registry.register_decorator(_("Tests"), lambda request:
@@ -39,20 +36,14 @@ def is_any_tests_package_visible(request):
 @enforce_condition(not_anonymous & contest_exists & can_enter_contest)
 @enforce_condition(is_any_tests_package_visible)
 def tests_view(request, contest_id):
-    pis = ProblemInstance.objects.filter(contest=contest_id)
     tests = []
-    for pi in pis:
-        problem = pi.problem
-        tests_packages = TestsPackage.objects.filter(problem=problem)
-        round = pi.round
-        for tp in tests_packages:
-            if tp.is_visible(request.timestamp) and tp.package is not None:
-                t = {'name': os.path.basename(tp.name) + '.zip',
-                     'link': reverse('test', kwargs={'contest_id': contest_id,
-                         'package_id': tp.id}),
-                     'round': round,
-                     'description': tp.description}
-                tests.append(t)
+    for tp in visible_tests_packages(request):
+        t = {'name': os.path.basename(tp.name) + '.zip',
+             'link': reverse('test', kwargs={'contest_id': contest_id,
+                                             'package_id': tp.id}),
+             'round': round,
+             'description': tp.description}
+        tests.append(t)
     return TemplateResponse(request, 'testspackages/tests.html',
             {'tests': tests,
              'tests_on_page': getattr(settings, 'SUBMISSIONS_ON_PAGE', 100)})
