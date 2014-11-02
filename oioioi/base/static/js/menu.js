5dd09f48b4e13025e2f0d59d675cefa7e2dd20c0
Piotr Kozakowski <kozak000@gmail.com>
2014-11-02 01:53:20 +0100

    SIO-1524 Contestlogo should be hidden before the menu is hidden on short screens
    Change-Id: I56185856120139b331dc9cca68e21d9b7751bc7a

--- oioioi/base/static/js/menu.js
+++ oioioi/base/static/js/menu.js
@@ -12,8 +12,32 @@ $(window).load(function() {
     var submenu_divider = $('#nav-dropdown-divider');
     var footer = $('.body-with-menu footer');
 
+    // nonexp is a shorthand for nonexpandable
+    var nonexps = menu_div.children().not('.nav-list');
 
-    var hide_first = function() {
+    var last_visible_nonexp = function() {
+        return nonexps.filter(':visible:last');
+    };
+
+    var first_hidden_nonexp = function() {
+        return nonexps.filter(':hidden:first');
+    };
+
+    var can_hide = function() {
+        return last_visible_nonexp().length || last_li().length;
+    };
+
+    var can_show = function() {
+        return first_hidden_nonexp().length || submenu_first_li().length;
+    };
+
+    var hide_last = function() {
+        var lvn = last_visible_nonexp();
+        if (lvn.length == 1) {
+            lvn.hide();
+            return;
+        }
+
         if (submenu_ul.is(':empty')) {
             submenu_divider.show();
             submenu.show();
@@ -22,12 +46,23 @@ $(window).load(function() {
         last_li().prependTo(submenu_ul);
     };
 
-    var show_first = function() {
-        submenu_first_li().insertBefore(submenu_divider);
-        if (submenu_ul.is(':empty')) {
-            submenu_divider.hide();
-            submenu.hide();
+    var show_last = function() {
+        var sfl = submenu_first_li();
+        if (sfl.length == 1) {
+            sfl.insertBefore(submenu_divider);
+            if (submenu_ul.is(':empty')) {
+                submenu_divider.hide();
+                submenu.hide();
+            }
+            return;
         }
+
+        first_hidden_nonexp().show();
+    };
+
+    var last_visible_end = function() {
+        var last_visible = menu_div.children(':visible:last');
+        return last_visible.offset().top + last_visible.height();
     };
 
     var recalculate_menu = function() {
@@ -37,15 +72,11 @@ $(window).load(function() {
         var cut_y = footer.length ? footer.offset().top
                 : menu_div.offset().top + $(window).height();
 
-        while (submenu_first_li().length == 1 &&
-                menu_ul.offset().top + menu_ul.height() < cut_y) {
-            show_first();
-        }
+        while (can_show() && last_visible_end() < cut_y)
+            show_last();
 
-        while (last_li().length == 1 &&
-                menu_ul.offset().top + menu_ul.height() > cut_y) {
-            hide_first();
-        }
+        while (can_hide() && last_visible_end() > cut_y)
+            hide_last();
     };
 
     recalculate_menu();
