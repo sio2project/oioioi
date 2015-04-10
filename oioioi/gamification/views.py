50bb793526399ff13a6ec256d8df4aa34ed861d1
Piotr Dulikowski <pd346907@students.mimuw.edu.pl>
2015-04-10 03:46:57 +0200

    SIO-1634 Code sharing GUI
    Change-Id: I0b6ddd94b2d942d862548688df1331244b4e8b18

--- oioioi/gamification/views.py
+++ oioioi/gamification/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
@@ -6,8 +7,10 @@ from django.template.response import TemplateResponse
 from django.utils.translation import ugettext_lazy as _
 
 from oioioi.base.menu import account_menu_registry
+from oioioi.gamification.controllers import CodeSharingController
 from oioioi.gamification.experience import Experience
 from oioioi.gamification.profile import profile_registry
+from oioioi.problems.problem_site import problem_site_tab
 
 
 @account_menu_registry.register_decorator(_("Profile"),
@@ -31,3 +34,14 @@ def profile_view(request, username=None):
         'exp_percentage': int(100 * exp.current_experience /
                               exp.required_experience_to_lvlup)
     })
+
+
+@problem_site_tab(_("Shared solutions"), key='shared_solutions', order=400)
+def problem_site_shared_solutions(request, problem):
+    controller = CodeSharingController()
+    submissions = controller.shared_with_me(problem, request.user)
+    return TemplateResponse(request,
+        'gamification/shared_submissions_tab.html',
+        {'submissions': submissions,
+         'submissions_on_page': getattr(settings, 'SUBMISSIONS_ON_PAGE', 20)}
+    )
