6d45c95cfa63255d79ab3fb5e8a8f74fc7920fba
Michał Adamczyk <mr.adamczyk@gmail.com>
2015-04-14 19:17:13 +0200

    (no-ticket) ONTAK participants should see the ranking
    Change-Id: I623ade1e72d6229e8188358af262558503bd9757

--- oioioi/ontak/controllers.py
+++ oioioi/ontak/controllers.py
@@ -11,13 +11,16 @@ class OntakContestController(OIContestController):
     def registration_controller(self):
         return ParticipantsController(self.contest)
 
+    def can_see_ranking(self, request):
+        return True
 
-class OntakEternalController(OIContestController):
+    def should_confirm_submission_receipt(self, request, submission):
+        return False
+
+
+class OntakEternalController(OntakContestController):
     description = _("ONTAK-eternal")
     create_forum = False
 
     def registration_controller(self):
         return PublicContestRegistrationController(self.contest)
-
-    def can_see_ranking(self, request):
-        return True
