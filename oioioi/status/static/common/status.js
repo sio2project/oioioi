c0109c3af51e56a4305aaf0a0a1fd9b9e4408bec
Pawel Golinski <pg359186@students.mimuw.edu.pl>
2015-11-14 02:00:35 +0000

    SIO-1706 Add support for status requests in status module
    The new 'updateStatusRequest' event uses jQuery's Deferred API.
    
    Change-Id: I08d88c843317f5e62985fe13e248c9b1e79b899e

--- oioioi/status/static/common/status.js
+++ oioioi/status/static/common/status.js
@@ -1,13 +1,53 @@
 $(function(){
+    var STALE_PROMISE_TIME = 3000;
+
+    var promise_pending = null;
     var sync_time = 300000 + (Math.random()*60000)|0;
     var sync_interval;
     var status_url = oioioi_base_url + "status";
 
-    var fetchUpdates = function() {
-        $.getJSON(status_url, function(data) {
+    var update_status_promise = function() {
+        var data = null;
+
+        var dfd = $.Deferred()
+        .done(function() {
+            promise_pending = null;
             $(window).trigger('updateStatus', data);
+        })
+        .fail(function() {
+            // If we fail, we try to fullfill a promise again
+            promise_pending = null;
+            $(window).trigger('updateStatusRequest');
         });
+
+        var json_request = $.getJSON(status_url, function(aData) {
+            data = aData;
+        });
+
+        $.when(json_request)
+        .done(function() {
+            dfd.resolve();
+        })
+        .fail(function() {
+            dfd.reject();
+        });
+
+        setTimeout(function() {
+            json_request.abort();
+        }, STALE_PROMISE_TIME);
+
+        return dfd.promise();
     };
+
+    var fetchUpdates = function() {
+        $(window).trigger('updateStatusRequest');
+    };
+
+    $(window).on('updateStatusRequest', function() {
+        if (promise_pending === null) {
+            promise_pending = update_status_promise();
+        }
+    });
 
     $(window).one('initialStatus', function(ev, data) {
         if (data.sync_time) {
