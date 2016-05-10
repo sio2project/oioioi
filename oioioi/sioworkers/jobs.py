f000b6a9f1e0f5ff3eceef45ecea2038e0dc9f03
Piotr Wisniewski <pw361286@students.mimuw.edu.pl>
2016-05-10 20:54:41 +0200

    (SIO-1781) Create convenient solution for connecting checker to sio2 instance
    Url on which extern worker can access filetracker server is now added
    to the environ. The url can be set in settings.
    
    Change-Id: Ib94eee06de76153a1e73a6a4196a1f8220f816c4

--- oioioi/sioworkers/jobs.py
+++ oioioi/sioworkers/jobs.py
@@ -6,13 +6,20 @@ def _get_backend():
     return get_object_by_dotted_name(settings.SIOWORKERS_BACKEND)()
 
 
+# The url is added to job because worker needs to know which filetracker
+# server it should access.
 def run_sioworkers_job(job, **kwargs):
+    job['filetracker_url'] = settings.FILETRACKER_URL
     return _get_backend().run_job(job, **kwargs)
 
 
 def run_sioworkers_jobs(dict_of_jobs, **kwargs):
+    for _, env in dict_of_jobs.iteritems():
+        env['filetracker_url'] = settings.FILETRACKER_URL
     return _get_backend().run_jobs(dict_of_jobs, **kwargs)
 
 
 def send_async_jobs(dict_of_jobs, **kwargs):
+    for _, job in dict_of_jobs['workers_jobs'].iteritems():
+        job['filetracker_url'] = settings.FILETRACKER_URL
     return _get_backend().send_async_jobs(dict_of_jobs, **kwargs)
