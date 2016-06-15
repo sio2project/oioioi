9aab5aa973ce3cf2e05696013ab88f2de6bd9bc3
Stanisław Barzowski <sbarzowski@talent.edu.pl>
2016-06-15 11:22:42 +0200

    (no-ticket) Fix overflow issues and refactor js.
    Change-Id: I86494d3bfff253854324a8f79a8a421cbfe45ece

--- oioioi/portals/static/common/portal.js
+++ oioioi/portals/static/common/portal.js
@@ -1,5 +1,7 @@
 $(function() {
     var animationDuration = 500;
+    var delayDuration = 400;
+
     $('.portal-menu-siblings').each(function() {
         var self = $(this);
         var parent = $(this).parent();
@@ -7,57 +9,55 @@ $(function() {
         var chevron = parent.find('.icon-chevron-down');
         var inTimeout, outTimeout;
         var orientation = 0;
+
+        function rotateChevron(deg) {
+            chevron.stop(true, true);
+            $({deg: orientation}).animate({deg: deg}, {
+                step: function(now) {
+                    chevron.css({transform: 'rotate(' + now + 'deg)'});
+                    orientation = now;
+                },
+                duration: animationDuration
+            });
+        }
+
+        function showMenuSiblings() {
+            self.stop(true).css('display', 'block').animate({
+                left: '100%',
+                opacity: 1
+            }, animationDuration);
+        }
+
+        function hideMenuSiblings() {
+            self.stop(true).animate({
+                left: '0',
+                opacity: 0
+            }, animationDuration, 'swing', function() {
+                self.css('display', 'none');
+            });
+        }
+
         parent.hover(function() {
             a.addClass('hover');
 
+            // stop any hiding in progress
             clearTimeout(outTimeout);
-            inTimeout = setTimeout(function() {
-                self.stop(true).css('display', 'block').animate({
-                    left: '100%',
-                    opacity: 1
-                }, animationDuration);
 
-                chevron.stop(true, true);
-                $({deg: orientation}).animate({deg: -90}, {
-                    step: function(now) {
-                        chevron.css({transform: 'rotate(' + now + 'deg)'});
-                        orientation = now;
-                    },
-                    duration: animationDuration
-                });
-            }, 400);
+            // it feels more natural with a slight delay
+            inTimeout = setTimeout(function() {
+                showMenuSiblings();
+                rotateChevron(-90);
+            }, delayDuration);
         }, function() {
             a.removeClass('hover');
+
+            // stop any showing in progress
             clearTimeout(inTimeout);
 
             outTimeout = setTimeout(function() {
-                self.stop(true).animate({
-                    left: '0',
-                    opacity: 0
-                }, animationDuration);
-                outTimeout = setTimeout(function() {
-                    self.css('display', 'none');
-                }, animationDuration);
-
-                chevron.stop(true, true);
-                $({deg: orientation}).animate({deg: 0}, {
-                    step: function(now) {
-                        chevron.css({transform: 'rotate(' + now + 'deg)'});
-                        orientation = now;
-                    },
-                    duration: animationDuration
-                });
-            }, 400);
+                hideMenuSiblings();
+                rotateChevron(0);
+            }, delayDuration);
         });
-
-        var url = a.attr('href');
-        var height = parent.height() + 1;
-        var top = -3;
-        $(this).children('li').each(function() {
-            if($(this).children('a').attr('href') == url)
-                return false;
-            top -= height;
-        });
-        $(this).css('top', top + 'px');
     });
 });
