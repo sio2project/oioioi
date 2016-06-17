5d55eeca66b600811f49a769f40dfd134ca7eff5
Pawel Golinski <golpaw1@gmail.com>
2016-06-17 22:56:15 +0200

    SIO-1829 Add formatting to newsfeed
    Change-Id: Ib63b52533e940216ccfdb8d81130e4140619959a

--- oioioi/newsfeed/models.py
+++ oioioi/newsfeed/models.py
@@ -1,5 +1,7 @@
+from mistune import Markdown
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from django.utils.safestring import mark_safe
 
 from oioioi.base.utils.deps import check_django_app_dependencies
 
@@ -11,3 +13,6 @@ class News(models.Model):
     date = models.DateTimeField(auto_now_add=True, verbose_name=_("date"))
     title = models.CharField(max_length=255, verbose_name=_("title"))
     content = models.TextField(verbose_name=_("content"))
+
+    def rendered_content(self):
+        return mark_safe(Markdown(escape=True).render(self.content))
