f74e33e969c46b1906272cee8f4fa857219f25f2
Marcin Mielniczuk <marmistrz.dev@gmail.com>
2016-12-14 15:10:37 +0100

    SIO-1824 Move the ContestSelectionWidget to oioioi.teachers
    Change-Id: I998d56736d44a291c0aa695236e8c9feea0c2651

--- oioioi/portals/widgets.py
+++ oioioi/portals/widgets.py
@@ -5,16 +5,12 @@ from mistune import Renderer, InlineGrammar, InlineLexer, Markdown, BlockLexer
 from django.conf import settings
 from django.core.urlresolvers import resolve, reverse
 from django.http import Http404
-from django.template import RequestContext
 from django.template.loader import render_to_string
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
 
-from oioioi.contests.utils import visible_contests
-from oioioi.contests.processors import recent_contests
 from oioioi.contests.models import UserResultForProblem
 from oioioi.contests.views import submission_view
-from oioioi.teachers.models import Teacher
 from oioioi.problems.models import Problem
 from oioioi.problems.views import problem_site_view
 
@@ -203,34 +199,3 @@ class ProblemTableWidget(object):
         return render_to_string('portals/widgets/problem_table.html',
                                 {'problems': rows, 'header': header})
 register_widget(ProblemTableWidget())
-
-
-class ContestSelectionWidget(object):
-    name = 'contest_selection'
-    compiled_tag_regex = re.compile(r'\[\[ContestSelection\]\]')
-    TO_SHOW = getattr(settings, 'NUM_RECENT_CONTESTS', 5)
-
-    def render(self, request, m):
-        rcontests = recent_contests(request)
-        contests = list(visible_contests(request).difference(rcontests))
-        contests.sort(key=lambda x: x.creation_date, reverse=True)
-        contests = (rcontests + contests)[:self.TO_SHOW+1]
-
-        default_contest = None
-        if rcontests:
-            default_contest = rcontests[0]
-        elif contests:
-            default_contest = contests[0]
-
-        context = {
-            'contests': contests[:self.TO_SHOW],
-            'default_contest': default_contest,
-            'more_contests': len(contests) > self.TO_SHOW,
-            'is_teacher': request.user.has_perm('teachers.teacher'),
-            'is_inactive_teacher':
-            request.user.is_authenticated() and
-            bool(Teacher.objects.filter(user=request.user, is_active=False))
-        }
-        return render_to_string('portals/widgets/contest_selection.html',
-                                RequestContext(request, context))
-register_widget(ContestSelectionWidget())
---
+++ oioioi/teachers/widgets.py
@@ -0,0 +1,41 @@
+import re
+
+from django.conf import settings
+from django.template.loader import render_to_string
+from django.template import RequestContext
+
+from oioioi.contests.utils import visible_contests
+from oioioi.contests.processors import recent_contests
+from oioioi.teachers.models import Teacher
+from oioioi.portals.widgets import register_widget
+
+
+class ContestSelectionWidget(object):
+    name = 'contest_selection'
+    compiled_tag_regex = re.compile(r'\[\[ContestSelection\]\]')
+    TO_SHOW = getattr(settings, 'NUM_RECENT_CONTESTS', 5)
+
+    def render(self, request, m):
+        rcontests = recent_contests(request)
+        contests = list(visible_contests(request).difference(rcontests))
+        contests.sort(key=lambda x: x.creation_date, reverse=True)
+        contests = (rcontests + contests)[:self.TO_SHOW+1]
+
+        default_contest = None
+        if rcontests:
+            default_contest = rcontests[0]
+        elif contests:
+            default_contest = contests[0]
+
+        context = {
+            'contests': contests[:self.TO_SHOW],
+            'default_contest': default_contest,
+            'more_contests': len(contests) > self.TO_SHOW,
+            'is_teacher': request.user.has_perm('teachers.teacher'),
+            'is_inactive_teacher':
+            request.user.is_authenticated() and
+            bool(Teacher.objects.filter(user=request.user, is_active=False))
+        }
+        return render_to_string('portals/widgets/contest_selection.html',
+                                RequestContext(request, context))
+register_widget(ContestSelectionWidget())
