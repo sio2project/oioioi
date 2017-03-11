1536c93a039d34ce22ae65ea6777376ff38c336c
mrowqa <artur.jamro@gmail.com>
2017-03-11 23:38:29 +0100

    (no-ticket) Fixed checking if Sioworkersd backend is enabled with spliteval.
    Change-Id: Ia0d9f44c26be305becaff4b9baa1967dc9a22882

--- oioioi/spliteval/__init__.py
+++ oioioi/spliteval/__init__.py
@@ -2,6 +2,6 @@ from django.conf import settings
 
 # SioworkersdBackend handles postponing checking by using priorities
 if settings.SIOWORKERS_BACKEND == 'oioioi.sioworkers.backends.Sioworkersd\
-        backend' and settings.ENABLE_SPLITEVAL:
+        Backend' and settings.ENABLE_SPLITEVAL:
     raise AssertionError(
         "Please set ENABLE_SPLITEVAL to False when using Sioworkersd")
