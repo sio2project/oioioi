92e1946d4e804b91ceb443b1a393cb9d07e7ec72
Olaf <ol347066@students.mimuw.edu.pl>
2017-11-22 15:06:27 +0100

    SIO-2032 Problemset: tags rendered as html
    Change-Id: I83d7e334542b1902619b68b4f39a2fa7d85bce14

--- oioioi/problems/templatetags/tag.py
+++ oioioi/problems/templatetags/tag.py
@@ -1,4 +1,5 @@
 from django.template import Library
+from django.utils.html import format_html
 
 from oioioi.base.utils.tags import get_tag_colors
 
@@ -8,7 +9,10 @@ register = Library()
 @register.simple_tag
 def tag_label(tag):
     colors = get_tag_colors(tag.name)
-    return '<span title="%(name)s" class="label"' \
-            ' style="background-color: %(bgcolor)s; color: %(textcolor)s;"' \
-            '>%(name)s</span>' \
-            % {'name': tag.name, 'bgcolor': colors[0], 'textcolor': colors[1]}
+    return format_html(
+        '<span title="{name}" class="label" style="background-color: '
+        '{bgcolor}; color: {textcolor};">{name}</span>',
+        name=tag.name,
+        bgcolor=colors[0],
+        textcolor=colors[1]
+    )
