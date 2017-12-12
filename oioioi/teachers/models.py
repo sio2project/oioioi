5b35be9618aa348ef8a028d506e2faeaf2023e91
norbert_siwek <nbitxd@gmail.com>
2017-12-12 21:17:51 +0100

    (SIO-1719) Added assertion for checking if TeacherAuthBackend is enabled
    Change-Id: Iea8fc57585ad8283038383676259f8cb596886d1

--- oioioi/teachers/models.py
+++ oioioi/teachers/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 from oioioi.base.utils.deps import check_django_app_dependencies
 from oioioi.base.utils import generate_key
 from oioioi.contests.models import Contest
 
 check_django_app_dependencies(__name__, ['oioioi.participants'])
+
+if 'oioioi.teachers.auth.TeacherAuthBackend' \
+        not in settings.AUTHENTICATION_BACKENDS:
+    raise ImproperlyConfigured("When using teacher module "
+                               "you have to activate TeacherAuthBackend")
 
 
 class Teacher(models.Model):
