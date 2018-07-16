088de254405009a1d1551455cc285998d348f264
teqwve <teqwve@gmail.com>
2018-07-16 23:25:44 +0200

    (no-ticket) Add support for sinolpack++ format
    * support for extra_execution_args
    * support for extra_execution_files

--- oioioi/sinolpack/utils.py
+++ oioioi/sinolpack/utils.py
@@ -1,5 +1,45 @@
+import logging
+
 from oioioi.filetracker.utils import django_to_filetracker_path
 from oioioi.sinolpack.models import ExtraConfig, ExtraFile
+from oioioi.evalmgr import tasks
+
+logger = logging.getLogger(__name__)
+
+
+def add_extra_execution_files(environ):
+    try:
+        config = ExtraConfig.objects.get(
+            problem_id=environ.get('problem_id')
+        ).parsed_config
+    except ExtraConfig.DoesNotExist:
+        config = {}
+
+    lang = environ.get('language')
+    for _, job in environ.get('tests', {}).items():
+        extra_args = config.get('extra_execution_args', {}).get(lang, [])
+        if isinstance(extra_args, str):
+            extra_args = [extra_args]
+        # here we allow to pass an empty argument by setting it to "" as bool([""])=True
+        if extra_args:
+            job['extra_execution_args'] = extra_args
+
+        extra_file_names = config.get('extra_execution_files', {}).get(lang, [])
+        if isinstance(extra_file_names, str):
+            extra_file_names = [extra_file_names]
+        extra_file_names = set(extra_file_names)
+        if extra_file_names:
+            extra_files = ExtraFile.objects.filter(
+                problem_id=environ.get('problem_id'), name__in=extra_file_names
+            ).all()
+            if len(extra_file_names) != len(extra_files):
+                raise RuntimeError(
+                    'Did not find expected extra files: ' + ', '.join(extra_file_names)
+                )
+            job['extra_execution_files'] = dict(
+                (ef.name, django_to_filetracker_path(ef.file)) for ef in extra_files
+            )
+    return environ
 
 
 def add_extra_files(environ, problem, additional_args=None):
@@ -28,3 +68,23 @@ def add_extra_files(environ, problem, additional_args=None):
     environ['extra_files'] = dict(
         (ef.name, django_to_filetracker_path(ef.file)) for ef in extra_files
     )
+
+    if 'recipe' in environ:
+        try:
+            tasks.add_after_recipe_entry(
+                environ,
+                'collect_tests',
+                (
+                    'add_extra_execution_files',
+                    'oioioi.sinolpack.utils.add_extra_execution_files',
+                ),
+            )
+        except IndexError:
+            tasks.add_after_recipe_entry(
+                environ,
+                'make_test',
+                (
+                    'add_extra_execution_files',
+                    'oioioi.sinolpack.utils.add_extra_execution_files',
+                ),
+            )
