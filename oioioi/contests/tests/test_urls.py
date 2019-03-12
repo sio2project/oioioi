510d4fba70603f7ee0701c77d8318fd83ed847fc
Michał Sidor (Michcioperz) <public+git@meekchopp.es>
2019-03-12 21:31:47 +0100

    (SIO-2204) Stop using patterns in urls tests
    `patterns` is removed in Django 1.10.
    
    Change-Id: I09a3af9066e1ec0480cab414be5bbce5cafc3c6b

--- oioioi/contests/tests/test_urls.py
+++ oioioi/contests/tests/test_urls.py
@@ -1,31 +1,31 @@
-from django.conf.urls import include, patterns, url
+from django.conf.urls import include, url
 
 from oioioi import urls
 from oioioi.contests.urls import make_patterns
 
-contest_patterns = patterns('',
+contest_patterns = [
     url(r'^render_contest_id/$',
         'oioioi.contests.tests.tests.render_contest_id_view'),
-)
+]
 
-namespaced_patterns = patterns('',
+namespaced_patterns = [
     url(r'^namespaced_id/$',
         'oioioi.contests.tests.tests.print_contest_id_view',
         name='print_contest_id'),
-)
+]
 
-neutral_patterns = patterns('',
+neutral_patterns = [
     url(r'^id/$', 'oioioi.contests.tests.tests.print_contest_id_view',
         name='print_contest_id'),
     url(r'', include((namespaced_patterns, 'namespace'))),
-)
+]
 
 
-noncontest_patterns = patterns('',
+noncontest_patterns = [
     url(r'noncontest_id/$',
         'oioioi.contests.tests.tests.print_contest_id_view',
         name='noncontest_print_contest_id'),
-)
+]
 
 urlpatterns = make_patterns(neutral_patterns, contest_patterns,
         noncontest_patterns, urls.urlpatterns)
--- oioioi/disqualification/urls.py
+++ oioioi/disqualification/urls.py
@@ -1,6 +1,3 @@
-# pylint: disable=unused-import
-from django.conf.urls import patterns
-
 # Force loading views
 from oioioi.disqualification.views import disqualification_fragment
 
