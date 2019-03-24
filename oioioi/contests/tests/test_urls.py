394d4966bcae8f8dde0b136e3214b5f56423ce01
Artur Puzio <ap395026@students.mimuw.edu.pl>
2019-03-24 19:38:30 +0100

    (SIO-2206) Pass callable to url() in tests
    This fixes following warning:
    
    RemovedInDjango110Warning: Support for string view arguments to url() is deprecated and will be removed in Django 1.10 (got oioioi.contests.tests.tests.print_contest_id_view). Pass the callable instead.
    
    Change-Id: Iebd76377e847396ab376bacd28320fb656dcc22c

--- oioioi/contests/tests/test_urls.py
+++ oioioi/contests/tests/test_urls.py
@@ -2,30 +2,28 @@ from django.conf.urls import include, url
 
 from oioioi import urls
 from oioioi.contests.urls import make_patterns
+from oioioi.contests.tests import tests
 
 contest_patterns = [
-    url(r'^render_contest_id/$',
-        'oioioi.contests.tests.tests.render_contest_id_view'),
+    url(r'^render_contest_id/$', tests.render_contest_id_view),
 ]
 
 namespaced_patterns = [
-    url(r'^namespaced_id/$',
-        'oioioi.contests.tests.tests.print_contest_id_view',
+    url(r'^namespaced_id/$', tests.print_contest_id_view,
         name='print_contest_id'),
 ]
 
 neutral_patterns = [
-    url(r'^id/$', 'oioioi.contests.tests.tests.print_contest_id_view',
+    url(r'^id/$', tests.print_contest_id_view,
         name='print_contest_id'),
     url(r'', include((namespaced_patterns, 'namespace'))),
 ]
 
 
 noncontest_patterns = [
-    url(r'noncontest_id/$',
-        'oioioi.contests.tests.tests.print_contest_id_view',
+    url(r'noncontest_id/$', tests.print_contest_id_view,
         name='noncontest_print_contest_id'),
 ]
 
 urlpatterns = make_patterns(neutral_patterns, contest_patterns,
-        noncontest_patterns, urls.urlpatterns)
+                            noncontest_patterns, urls.urlpatterns)
