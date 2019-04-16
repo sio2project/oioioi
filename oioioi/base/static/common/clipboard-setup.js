f73866571477640657c00899847270f39223baa3
Zuzanna Opała <opala.zuzanna@gmail.com>
2019-04-16 17:45:42 +0200

    (no-ticket) Fix translations inside compress
    Change-Id: Ib9025601d0b987a1faf88ef5bc063342339e2684

---
+++ oioioi/base/static/common/clipboard-setup.js
@@ -0,0 +1,6 @@
+new Clipboard('.btn-copy')
+    .on('success', function(e) {
+        e.trigger.outerHTML = '<small><span class="glyphicon glyphicon-ok"></span>' + gettext("copied!") + '</small>';
+    }).on('error', function(e) {
+        e.trigger.outerHTML = '<small>' + gettext("Press Ctrl+C to copy") + '</small>';
+    });
