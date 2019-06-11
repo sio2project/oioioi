07672edce721614968005cc0648f30814b0e1b06
Michal Sidor <public+git@meekchopp.es>
2019-06-11 18:18:45 +0200

    (SIO-2251) Replace notifications list with recent submissions list
    Change-Id: Ie115a4cf63b9ea4032aebafbaf91e8a067484171

--- oioioi/notifications/static/common/notifications.js
+++ oioioi/notifications/static/common/notifications.js
@@ -38,27 +38,27 @@ TranslationClient.prototype.translate = function (originalText) {
 let translationClient = new TranslationClient();
 
 function NotificationsClient(serverUrl, sessionId) {
-    this.NUMBER_BADGE_ID = "#notifications_number";
-    this.TABLE_NOTIFICATIONS_ID = "#balloon_table_notifications";
-    this.NO_NOTIFICATIONS_ID = "#info_no_notifications";
+    this.TABLE_NOTIFICATIONS_ID = "#notifications_table";
+    this.HEADER_REFRESH_ID = "#notifications_refresh";
     this.DROPDOWN_ID = "#notifications_dropdown";
     this.DROPDOWN_PANEL_ID = "#notifications_panel";
-    this.NUMBER_BADGE = $(this.NUMBER_BADGE_ID);
+    this.DROPDOWN_ICON_ID = "#notifications_icon";
+    this.DROPDOWN_DISCONNECTED_ID = "#notifications_error";
     this.TABLE_NOTIFICATIONS = $(this.TABLE_NOTIFICATIONS_ID);
-    this.NO_NOTIFICATIONS = $(this.NO_NOTIFICATIONS_ID);
+    this.HEADER_REFRESH = $(this.HEADER_REFRESH_ID);
+    this.HEADER_REFRESH_SPINNER = this.HEADER_REFRESH.find("span");
     this.DROPDOWN_PANEL = $(this.DROPDOWN_PANEL_ID);
     this.DROPDOWN = $(this.DROPDOWN_ID);
+    this.DROPDOWN_ICON = $(this.DROPDOWN_ICON_ID);
+    this.DROPDOWN_DISCONNECTED = $(this.DROPDOWN_DISCONNECTED_ID);
     this.DEBUG = true;
     this.NOTIF_SERVER_URL = serverUrl;
     this.NOTIF_SESSION = sessionId;
-    this.CONTENT_ENTRY = '<li class="%(notclass)s"><a href="%(address)s" ' +
-        'id="notif_msg_%(id)s">...' +
-        '<span class="notification-details">%(details)s</span>' +
-        '<time class="notification-time">%(time)s</time></a></li>';
     this.socket = undefined;
     this.notifCount = 0;
-    this.messages = [];
-    this.renderSuspended = false;
+
+    this.dropdownUpToDate = false;
+    this.dropdownLoading = false;
 
     this.openNotifications = [];
     this.waitingPermissions = [];
@@ -66,20 +66,29 @@ function NotificationsClient(serverUrl, sessionId) {
 
     if (typeof(io) === 'undefined') {
         this.setErrorState();
+        this.DROPDOWN_DISCONNECTED.show();
         return;
     }
 
-    this.renderMessages();
-    $(this.DROPDOWN).on("click", this.acknowledgeMessages.bind(this));
+    this.DROPDOWN_PANEL.on("show.bs.dropdown", this.renderMessages.bind(this));
+    this.DROPDOWN_PANEL.on("shown.bs.dropdown", this.acknowledgeMessages.bind(this));
+    this.DROPDOWN_PANEL.on("hidden.bs.dropdown", this.acknowledgeMessages.bind(this));
+    this.DROPDOWN_PANEL.find(".dropdown-menu").click((ev) => {
+        ev.stopPropagation();
+    });
+    this.HEADER_REFRESH.click(() => {
+        if (this.dropdownLoading)
+            return;
+        this.dropdownUpToDate = false;
+        this.renderMessages();
+    });
     this.socket = io.connect(this.NOTIF_SERVER_URL);
     this.socket.on('connect', this.authenticate.bind(this));
     this.socket.emits = function(k, v) {
         this.socket.emit(k, JSON.stringify(v));
     }.bind(this);
     setInterval(this.notifWatchdog.bind(this), 2000);
-    this.storage = new MessageManager(this);
-    this.socket.on("message", this.storage.onMessageReceived.bind(this.storage));
-
+    this.socket.on("message", this.onMessageReceived.bind(this));
 }
 
 NotificationsClient.prototype.constructor = NotificationsClient;
@@ -87,19 +96,19 @@ NotificationsClient.prototype.constructor = NotificationsClient;
 NotificationsClient.prototype.notifWatchdog = function() {
     if (!this.socket || !this.socket.connected) {
         this.setErrorState();
+        this.DROPDOWN_DISCONNECTED.show();
     }
 };
 
 NotificationsClient.prototype.clearNumberBadgeClasses = function() {
-    this.NUMBER_BADGE.removeClass("label-default");
-    this.NUMBER_BADGE.removeClass("label-success");
-    this.NUMBER_BADGE.removeClass("label-warning");
+    this.DROPDOWN.removeClass("btn btn-danger");
+    this.DROPDOWN_ICON.removeClass("text-warning");
+    this.DROPDOWN_DISCONNECTED.hide();
 };
 
 NotificationsClient.prototype.setErrorState = function() {
-    this.NUMBER_BADGE.text('!');
     this.clearNumberBadgeClasses();
-    this.NUMBER_BADGE.addClass("label-warning");
+    this.DROPDOWN_ICON.addClass("text-warning");
 };
 
 NotificationsClient.prototype.authenticate = function() {
@@ -110,73 +119,52 @@ NotificationsClient.prototype.authenticate = function() {
     {
         if (result.status !== 'OK') {
             me.setErrorState();
+            me.DROPDOWN_DISCONNECTED.show();
         }
         else {
             me.notifCount = 0;
             me.updateNotifCount();
-            me.storage.notifyClientOfOfflineMessages();
         }
     });
 };
 
 NotificationsClient.prototype.updateNotifCount = function() {
-    this.NUMBER_BADGE.text(this.notifCount);
     this.clearNumberBadgeClasses();
-    this.NUMBER_BADGE.addClass(this.notifCount > 0 ? "label-success"
-        : "label-default");
+    if (this.notifCount > 0)
+        this.DROPDOWN.addClass("btn btn-danger");
 };
 
 NotificationsClient.prototype.renderMessages = function() {
-    if (this.renderSuspended)
+    if (this.dropdownUpToDate || this.dropdownLoading)
         return;
-    const msgs = this.messages;
-    let content = '';
-    let wereMessages;
-    let msgsSorted = Object.keys(this.messages)
-         .sort(function(a,b) {
-            return Number(msgs[b].date) - Number(msgs[a].date);
+    this.dropdownLoading = true;
+    this.dropdownUpToDate = true;
+    this.HEADER_REFRESH_SPINNER.addClass("spinner");
+    $.get('/latest_submissions/', {}, 'html')
+        .success((data) => {
+            this.dropdownLoading = false;
+            this.HEADER_REFRESH_SPINNER.removeClass("spinner");
+            this.TABLE_NOTIFICATIONS.html(data);
+            if (this.TABLE_NOTIFICATIONS.find("tbody tr").length < 1) {
+                let columnsCount = this.TABLE_NOTIFICATIONS.find("thead th").length;
+                this.TABLE_NOTIFICATIONS.find("tbody").append(
+                    $("<tr></tr>").append(
+                        $("<td></td>").attr('colspan', columnsCount)
+                            .addClass("text-center")
+                            .text(gettext("You don't have any submissions."))
+                    )
+                );
+            }
+            this.dropdownLoading = false;
+        }).fail((err) => {
+            console.warn(err);
+            this.dropdownLoading = false;
+            this.HEADER_REFRESH_SPINNER.removeClass("spinner");
+            this.setErrorState();
+            let text = gettext("A network error occured. Recent submissions list could not be loaded");
+            this.TABLE_NOTIFICATIONS.text(text);
         });
-
-    for (let msgKey of msgsSorted) {
-        wereMessages = true;
-        const msg = this.messages[msgKey];
-        content += interpolate(this.CONTENT_ENTRY,
-            {
-                address: msg.address || '#',
-                id: msg.id,
-                details: msg.details || '',
-                notclass: msg.cached ? "notification" : "notification-new",
-                time: notifs_getDateTimeOf(msg.date)
-            }, true);
-
-    }
-
-    this.TABLE_NOTIFICATIONS.html(content);
-    for (let msgKey of msgsSorted) {
-        translationClient.translate(this.messages[msgKey].message)
-            .catch(() => this.messages[msgKey].message)
-            .then((msg) => {
-                let text = interpolate(msg, this.messages[msgKey].arguments, true);
-                $('#notif_msg_' + msgKey).text(text);
-            });
-    }
-    if (!wereMessages) {
-        this.TABLE_NOTIFICATIONS.append(this.NO_NOTIFICATIONS);
-    }
 };
-
-function notifs_getDateTimeOf(timeOfMsg) {
-    const t = function(time) { return time < 10 ? "0" + time : time; };
-    const dateOfMsg = new Date(timeOfMsg);
-
-    if (Date.now() - timeOfMsg < 24 * 60 * 60 * 1000) {
-        return interpolate("%s:%s", [t(dateOfMsg.getHours()), t(dateOfMsg.getMinutes())]);
-    }
-    else {
-        return interpolate("%s-%s-%s %s:%s", [t(dateOfMsg.getDate()), t(dateOfMsg.getMonth()),
-            dateOfMsg.getYear(), t(dateOfMsg.getHours()), t(dateOfMsg.getMinutes())]);
-    }
-}
 
 NotificationsClient.prototype.generatePermissionBanner = function () {
     return $("<div></div>").attr("id", "notifications_banner")
@@ -230,42 +218,38 @@ NotificationsClient.prototype.askPermission = function () {
     return Promise.reject("Notifications support missing");
 };
 
-NotificationsClient.prototype.onMessageReceived = function(message, cached) {
-    if (this.messages[message.id]) {
-        return;
-    }
-    if (cached) {
-        message.cached = true;
-    }
-    this.messages[message.id] = message;
-    this.renderMessages();
-    if (!cached) {
-        this.notifCount++;
-        this.updateNotifCount();
-        $.localStorage.set("notif_" + message.date + "_" + message.id, message);
-        if (message.popup && !$(this.DROPDOWN_PANEL).hasClass('open')) {
-            $(this.DROPDOWN).dropdown('toggle');
-        }
-
-        this.askPermission().then(() => {
-            return translationClient.translate(message.message).catch(() => message.message);
-        }).then((translation) => {
-            let content = interpolate(translation, message.arguments, true);
-            let notification = new Notification(content,
-                {tag: message.id, timestamp: message.date, body: content});
-            if (message.address) {
-                notification.addEventListener('click', (ev) => {
-                    ev.preventDefault();
-                    window.open(message.address, '_blank');
-                });
-            }
-            this.openNotifications.push(notification);
-        }).catch((err) => {
-            console.warn("Failed to display notification.", err);
-        });
-    }
+NotificationsClient.prototype.onMessageReceived = function(message) {
     if (this.DEBUG) {
         console.log('Received message:', message);
+    }
+
+    if (message.type == 'submission_judged' || message.type == 'initial_results') {
+        this.dropdownUpToDate = false;
+        this.notifCount++;
+        this.updateNotifCount();
+        if (message.popup && !this.DROPDOWN_PANEL.hasClass('open')) {
+            $(this.DROPDOWN).dropdown('toggle');
+        }
+    }
+
+    this.askPermission().then(() => {
+        return translationClient.translate(message.message).catch(() => message.message);
+    }).then((translation) => {
+        let content = interpolate(translation, message.arguments, true);
+        let notification = new Notification(content,
+            {tag: message.id, timestamp: message.date, body: content});
+        if (message.address) {
+            notification.addEventListener('click', (ev) => {
+                ev.preventDefault();
+                window.open(message.address, '_blank');
+            });
+        }
+        this.openNotifications.push(notification);
+    }).catch((err) => {
+        console.warn("Failed to display notification.", err);
+    });
+    if (this.DROPDOWN_PANEL.hasClass("open")) {
+        this.renderMessages();
     }
 };
 
@@ -276,37 +260,4 @@ NotificationsClient.prototype.acknowledgeMessages = function() {
         notif.close();
     });
     this.openNotifications = [];
-};
-
-function MessageManager(notificationsClient) {
-    this.client = notificationsClient;
-}
-
-MessageManager.prototype.cleanCache = function() {
-    const dateHourAgo = Date.now() - 60 * 60 * 1000;
-    const keys = $.localStorage.keys();
-    for (let key of keys) {
-        let keyInfo = key.split('_');
-        if (keyInfo[0] !== "notif")
-            continue;
-        if (Number(keyInfo[1]) < dateHourAgo)
-            $.localStorage.remove(key);
-    }
-};
-
-MessageManager.prototype.onMessageReceived = function(message) {
-    this.client.onMessageReceived(message);
-};
-
-MessageManager.prototype.notifyClientOfOfflineMessages = function() {
-    this.cleanCache();
-    this.client.renderSuspended = true;
-    const keys = $.localStorage.keys().filter(function(k) {
-        return k.indexOf("notif") === 0;
-    });
-    for (let key of keys) {
-        this.client.onMessageReceived($.localStorage.get(key), true);
-    }
-    this.client.renderSuspended = false;
-    this.client.renderMessages();
 };
