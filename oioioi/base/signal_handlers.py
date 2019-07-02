0b56fbc153f571d65edbfaf99a70328f145c95be
Szymon Acedański <accek@mimuw.edu.pl>
2019-07-02 15:36:58 +0200

    (no ticket) Workaround for Django bug #29182
    Which made OIOIOI incompatible with newest SQLite, and as a result
    many tests failing.
    
    Change-Id: I5c58c0af4250474df2b24b9add6c9ac2349491af

--- oioioi/base/signal_handlers.py
+++ oioioi/base/signal_handlers.py
@@ -1,7 +1,18 @@
+import django
 from django.contrib.auth.signals import user_logged_in
+from django.db.backends.signals import connection_created
+from django.dispatch import receiver
 
 
+@receiver(user_logged_in)
 def set_first_view_after_logging_flag(sender, user, request, **kwargs):
     request.session['first_view_after_logging'] = True
 
-user_logged_in.connect(set_first_view_after_logging_flag)
+
+# Workaround for https://code.djangoproject.com/ticket/29182
+# TODO: delete after upgrade to Django 2.1+
+assert django.VERSION < (2, 1, 5)
+@receiver(connection_created)
+def db_connection_callback(sender, connection, **kwargs):
+    if connection.vendor == 'sqlite':
+        connection.cursor().execute('PRAGMA legacy_alter_table = ON')
