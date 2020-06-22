08d2a9e0503420c297e161ad50ae4b807be848f5
Przemysław <pp406298@students.mimuw.edu.pl>
2020-06-22 20:41:57 +0200

    (no-ticket) Tweak visibility settings of submit view's delete button
    The button is now visible only when there is a file that can be deleted.
    
    Change-Id: I2cc327af105609b2a5be5ef3116a62a2fa1b27b8

--- oioioi/programs/static/common/cancellable_file_input.js
+++ oioioi/programs/static/common/cancellable_file_input.js
@@ -2,10 +2,28 @@
  *  Cancels file input after clicking the 'x' button.
  */
 $(function() {
-    const cancel = $('.file_cancel_button');
-    cancel.click(function() {
+    const cancelFields = $('.file_cancel_button');
+    const fileInputs = $('input', cancelFields.parent());
+
+    cancelFields.click(function() {
         const file = $('input', $(this).parent());
+
         file.val('');
         file.trigger('input');
     });
+
+    function cancelButtonShowHide() {
+        const file = $(this);
+        const cancel = $('.file_cancel_button', file.parent());
+
+        if (file.val())
+            cancel.show();
+        else
+            cancel.hide();
+    }
+
+    if (fileInputs.length) {
+        fileInputs.on('input', cancelButtonShowHide);
+        fileInputs.each(cancelButtonShowHide);
+    }
 });
