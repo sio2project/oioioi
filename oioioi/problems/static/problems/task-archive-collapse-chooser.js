f40902665fc13aa7a92722e989d473d3abf024e7
Hubert Badocha <h.badocha@student.uw.edu.pl>
2021-01-20 20:32:06 +0100

    SIO-2318 Add more detailed URLs and enable easy copying of them
    Added functionality to uncollapse selected in link category(# syntax).
    Added functionality to easily copy link to selected category.
    
    Change-Id: If6fe1ef0f8f8403357bdcae3520abb9c9e0b5e34

---
+++ oioioi/problems/static/problems/task-archive-collapse-chooser.js
@@ -0,0 +1,83 @@
+function performActionAfterTooltipCloses(el, fn) {
+
+    const tooltipId = el.getAttribute("aria-describedby");
+    const tooltipEl = document.getElementById(tooltipId);
+
+    const observer = new MutationObserver(function (records) {
+        const removedEls = Array.from(records.pop().removedNodes);
+        if (removedEls.includes(tooltipEl)) {
+            fn();
+            observer.disconnect();
+        }
+    });
+    observer.observe(tooltipEl.parentElement, {attributes: false, childList: true});
+}
+
+function createLink(link) {
+    const url = new URL(window.location.href);
+    url.hash = link;
+    return url.toString();
+}
+
+function showMessage(el) {
+    const prevTitle = el.dataset.originalTitle;
+    el.title = "Copied link!";
+    $(el).tooltip('fixTitle').tooltip('show');
+
+    const restoreTitle = function () {
+        el.title = prevTitle;
+        $(el).tooltip('fixTitle').tooltip('hide');
+    };
+
+    setTimeout(function () {
+        performActionAfterTooltipCloses(el, restoreTitle);
+    }, 0);
+
+}
+
+function copyLink(link) {
+    const input = document.createElement("input");
+    document.body.appendChild(input);
+    input.type = "text";
+    input.value = link;
+
+    // https://www.w3schools.com/howto/howto_js_copy_clipboard.asp
+
+    // Select the text field
+    input.select();
+    input.setSelectionRange(0, 99999);  // To ensure mobile devices compatibility,
+    // we use setSelectionRange instead select().
+    // selectionEnd value is chosen to be big enough to store any possible input.
+
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+    document.body.removeChild(input);
+}
+
+function enableCopy() {
+    $(document).on('click', '.link-getter', function () {
+        const link = createLink(this.dataset.link);
+        copyLink(link);
+        location.replace(link);
+        showMessage(this);
+    });
+}
+
+function chooseActive() {
+    const hash = location.hash;
+
+    if (hash) {
+        const el = document.querySelector('a[href="' + hash + '"]');
+        if (el) {
+            el.click();
+        }
+    }
+}
+
+function initLinks() {
+    chooseActive();
+    enableCopy();
+}
+
+window.onload = initLinks;
+
