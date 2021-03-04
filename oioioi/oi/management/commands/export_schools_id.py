d9890d3d10946a1ef0ce0807a1d4d6431a3b2d94
sio2 <sio2@dasie.mimuw.edu.pl>
2021-03-04 23:03:19 +0100

    (no-ticket) Add script to export schools with ids
    Change-Id: Iee7831cda6f3327edc11c5cfa9cb102f91b264b1

---
+++ oioioi/oi/management/commands/export_schools_id.py
@@ -0,0 +1,25 @@
+import csv
+
+import six
+import sys
+from django.core.management.base import BaseCommand
+from django.utils.translation import ugettext as _
+
+from oioioi.oi.management.commands.import_schools import COLUMNS
+from oioioi.oi.models import School
+
+COLUMNS = ['id'] + COLUMNS
+
+class Command(BaseCommand):
+    help = _("Exports schools list to a CSV file")
+
+    requires_model_validation = True
+
+    def handle(self, *args, **options):
+        writer = csv.writer(sys.stdout)
+        writer.writerow(COLUMNS)
+        schools = School.objects.filter(is_approved=True, is_active=True)
+        for school in schools.order_by('postal_code'):
+            row = [six.text_type(getattr(school, column)).encode('utf-8')
+                    for column in COLUMNS]
+            writer.writerow(row)
