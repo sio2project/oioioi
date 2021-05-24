d7b0c62f700fbb188a1ee9ad2a992b3c6cf8b1b3
Szymon <szymon.czyzmanski@gmail.com>
2021-05-24 14:00:59 +0200

    (no-ticket) Fix matching OriginInfoValue objects with OriginTag objects
    In the problem settings admin site, it is possible to tag the given
    problem with OriginTag objects and their corresponding OriginInfoValue
    objects. OriginInfoValue objects displayed in the drop-down list
    should be limited only to those that have their parent_tag field
    equal to some picked OriginTag object. Due to the change in displaying
    OriginTag and OriginInfoValue objects (full name displayed instead of name),
    JavaScript code couldn't match any OriginInfoValue object with any OriginTag
    object. During the development process, it turned out that another bug
    was present - if some picked OriginTag was removed, OriginInfoValue objects
    displayed were not refreshed to reflect updated picked OriginTag objects set.
    
    *Use OriginInfoValue's full name in the lookup for parent OriginTag.
    *Invoke existing on_change() function when a form is removed from the
     exisiting formset - OriginTag_problems.
    *Refactor old JavaScript code.
    
    Change-Id: Idbf7eb386504b39195a40fecd72419aec27e11cb

--- oioioi/problems/static/problems/admin-origintag.js
+++ oioioi/problems/static/problems/admin-origintag.js
@@ -21,15 +21,17 @@ $(document).ready(function() {
                 let option = $(this).text();
                 let has_origintag = false;
                 tag_values.forEach(function(tag_value, i, a) {
-                    if (tag_value != "" && option.startsWith(tag_value + '_'))
+                    if (tag_value !== "" && option.startsWith(tag_value + ' ')) {
                         has_origintag = true;
+                    }
                 });
-                if (option == null_option || has_origintag) {
+                if (option === null_option || has_origintag) {
                     $(this).show();
                 } else {
                     $(this).hide();
-                    if ($(this).val() == value_select.val())
+                    if ($(this).val() === value_select.val()) {
                         value_select.val('');
+                    }
                 }
             });
             $(origininfovalues).change();
@@ -39,15 +41,17 @@ $(document).ready(function() {
     // If any row gets added we need to setup a new trigger
     django.jQuery(document).on('formset:added',
                                function(event, $row, formsetName) {
-        if (formsetName == 'OriginTag_problems')
+        if (formsetName === 'OriginTag_problems') {
             $row.find(origintags).on('change', on_change);
+        }
     });
 
     // If an origin tag is removed we need to recalculate possible origin meta
     django.jQuery(document).on('formset:removed',
                                function(event, $row, formsetName) {
-        if (formsetName == 'origintagthrough_set')
-            on_origintag_change();
+        if (formsetName === 'OriginTag_problems') {
+            on_change();
+        }
     });
 
     // Setup triggers and calculate initial values
@@ -58,8 +62,8 @@ $(document).ready(function() {
     // Based on: https://stackoverflow.com/a/33937138
 
     function triggerChangeOnField(win) {
-        var name = windowname_to_id(win.name);
-        var elem = document.getElementById(name);
+        const name = windowname_to_id(win.name);
+        const elem = document.getElementById(name);
         $(elem).change();
     }
 
@@ -67,13 +71,13 @@ $(document).ready(function() {
         return function(win, objId, newRepr, newId) {
             fn(win, objId, newRepr, newId);
             triggerChangeOnField(win);
-        }
+        };
     } (window.dismissChangeRelatedObjectPopup);
 
     window.dismissAddRelatedObjectPopup = function(fn) {
         return function(win, newId, newRepr) {
             fn(win, newId, newRepr);
             triggerChangeOnField(win);
-        }
+        };
     } (window.dismissAddRelatedObjectPopup);
 });
