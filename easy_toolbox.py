b4289c9af2d7a54d5f46855525ddb56118221be1
Aleksander Tudruj <aleksanderwt@gmail.com>
2022-05-17 14:56:41 +0200

    cypress action first test

---
+++ easy_toolbox.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python3
+
+# pip requirements:
+#   python ^3.6
+#   inquirer
+# 
+# system:
+#   docker
+#   docker-compose
+
+
+# This script was created in order to help the users
+# execute commands faster. The main idea was to omit
+# copy-pasting commands from GitHub. This script
+# is prepared and should be upgraded or/and extended
+# for any future needs.
+
+import sys
+import os
+from tokenize import Number
+import inquirer
+
+
+BASE_DOCKER_COMMAND="OIOIOI_UID=$(id -u) docker-compose" + \
+     " -f docker-compose-dev.yml" + \
+     " -f extra/docker/docker-compose-dev-noserver.yml"
+
+
+RAW_COMMANDS = [
+    ("build", "Build whole OIOIOI from source.", "build", True),
+    ("up", "Run all SIO2 containers", "up -d"),
+    ("down", "Stop all SIO2 containers", "down"),
+    ("run", "Run server", "exec web python3 manage.py runserver 0.0.0.0:8000"),
+    ("bash", "Open command prompt on web container.", "exec web bash"),
+    ("bash_db", "Open command prompt on database container.", "exec db bash"),
+    # This one CLEARS the database. Use wisely.
+    ("flush-db", "Clear database.", "exec web python manage.py flush --noinput", True),
+    ("add-superuser", "Create admin_admin.", "exec web python manage.py createsuperuser"),
+    ("test", "Run unit tests.", "exec web ../oioioi/test.sh"),
+    ("test-slow", "Run unit tests. (--runslow)", "exec web ../oioioi/test.sh --runslow"),
+    ("test-abc", "Run specific test file. (edit the toolbox)", "exec web ../oioioi/test.sh -v oioioi/problems/tests/test_task_archive.py"),
+    ("test-coverage", "Run coverage tests.", "exec 'web' ../oioioi/test.sh oioioi/problems --cov-report term --cov-report xml:coverage.xml --cov=oioioi"),
+    ("server-cypress", "Run CyPress test server.", "exec web python manage.py testserver ../oioioi/oioioi_cypress/cypress/fixtures/admin_admin.json --no-input --addrport 0.0.0.0:8000 --settings oioioi.cypress_settings"),
+]
+
+
+longest_command_arg = max([len(command[0]) for command in RAW_COMMANDS])
+
+
+class Help(Exception):
+    pass
+
+
+class Option:
+    def __init__(self, arg, help, command, warn = False):
+        self.arg = arg
+        self.help = help
+        self.command = command
+        self.warn = warn
+    
+    def long_str(self) -> str:
+        return f"Option({self.arg}, Description='{self.help}', Command='{self.command}')"
+
+    def __str__(self) -> str:
+        spaces = longest_command_arg - len(self.arg)
+        return f"[{self.arg}] {' ' * spaces} {self.help}"
+
+
+COMMANDS = [Option(*x) for x in RAW_COMMANDS]
+
+
+def check_commands() -> None:
+    if len(set([opt.arg for opt in COMMANDS])) != len(COMMANDS):
+        raise Exception("Error in COMMANDS. Same name was declared for more then one command.")
+
+
+def get_action_from_args() -> Option:
+    if len(sys.argv) < 2:
+        return None
+    if len(sys.argv) > 2:
+        raise Exception("Too many arguments!")
+
+    if (sys.argv[1] in ['--help' ,'-h']):
+        raise Help()
+    
+    candidates = list(filter(lambda opt: opt.arg == sys.argv[1], COMMANDS))
+    if len(candidates) < 1:
+        raise Exception("No argument was found!")
+    if len(candidates) > 1:
+        raise Exception("More then one matching argument was found!")
+    
+    return candidates[0]
+
+
+def get_action_from_cli() -> Option:
+    questions = [
+        inquirer.List(
+            "action",
+            message="Select OIOIOI action",
+            choices=COMMANDS
+        )
+    ]
+    answers = inquirer.prompt(questions)
+    return answers["action"]
+
+
+def run_command(command) -> None:
+    print('Running command', command)
+    width = os.get_terminal_size().columns
+    print('=' * width)
+    os.system(command)
+
+
+def warn_user(action: Option) -> None:
+    print("You are going to execute command marked as `dangerous`. Are you sure? [y/N]")
+    while True:
+        choice = input().lower()
+        if len(choice) == 0 or "no"[:len(choice)] == choice:
+            return False
+        elif "yes"[:len(choice)] == choice:
+            return True
+        else:
+            print("Please answer [yes] or [no].")
+
+
+def run() -> None:
+    action = get_action_from_args() or get_action_from_cli()
+    if action.warn:
+        if not warn_user(action):
+            print("Aborting.")
+            return
+    run_command(f'{BASE_DOCKER_COMMAND} {action.command}')
+
+
+def print_help() -> None:
+    print("OIOIOI helper toolbox", "", "This script allows to control OIOIOI with Docker commands.",
+            f"Commands are always being run with '{BASE_DOCKER_COMMAND}' prefix.",
+            f"Aveliable commands are: ", "",
+            *COMMANDS, "", "Example `build`:", f"{sys.argv[0]} build", sep="\n")
+
+
+def main() -> None:
+    try:
+        check_commands()
+        run()
+    except Help:
+        print_help()
+    except Exception as e:
+        print(f"An error occurred during execution: {e}", file=sys.stderr)
+
+
+if __name__ == '__main__':
+    main()
--- oioioi/cypress_settings.py
+++ oioioi/cypress_settings.py
@@ -1,11 +1,28 @@
 # pylint: disable=wildcard-import
-from oioioi.default_settings import *
+from settings import *
 
-TIME_ZONE = 'UTC'
-
-SITE_ID = 1
-
-ADMINS = (('Test admin', 'admin@example.com'),)
+# Enable optional modules.
+INSTALLED_APPS = (
+    'oioioi.teachers',
+    'oioioi.ipdnsauth',
+    'oioioi.zeus',
+    'oioioi.oireports',
+    'oioioi.oisubmit',
+    'oioioi.complaints',
+    'oioioi.exportszu',
+    'oioioi.similarsubmits',
+    'oioioi.disqualification',
+    'oioioi.submitservice',
+    'oioioi.amppz',
+    'oioioi.teams',
+    'oioioi.pa',
+    'oioioi.notifications',
+    'oioioi.mailsubmit',
+    'oioioi.portals',
+    'oioioi.newsfeed',
+    'oioioi.simpleui',
+    'oioioi.livedata',
+) + INSTALLED_APPS
 
 DATABASES = {
     'default': {
@@ -14,63 +31,13 @@ DATABASES = {
     }
 }
 
-# Enable optional modules.
-INSTALLED_APPS = (
-    'oioioi.contestlogo',
-    'oioioi.teachers',
-    'oioioi.ipdnsauth',
-    'oioioi.participants',
-    'oioioi.oi',
-    'oioioi.printing',
-    'oioioi.zeus',
-    'oioioi.testrun',
-    'oioioi.scoresreveal',
-    'oioioi.oireports',
-    'oioioi.oisubmit',
-    'oioioi.szkopul',
-    'oioioi.complaints',
-    'oioioi.contestexcl',
-    'oioioi.forum',
-    'oioioi.exportszu',
-    'oioioi.plagiarism',
-    'oioioi.similarsubmits',
-    'oioioi.disqualification',
-    'oioioi.confirmations',
-    'oioioi.ctimes',
-    'oioioi.acm',
-    'oioioi.suspendjudge',
-    'oioioi.submitservice',
-    'oioioi.timeline',
-    'oioioi.statistics',
-    'oioioi.amppz',
-    'oioioi.balloons',
-    'oioioi.publicsolutions',
-    'oioioi.testspackages',
-    'oioioi.teams',
-    'oioioi.pa',
-    'oioioi.notifications',
-    'oioioi.mailsubmit',
-    'oioioi.globalmessage',
-    'oioioi.portals',
-    'oioioi.newsfeed',
-    'oioioi.simpleui',
-    'oioioi.livedata',
-    'oioioi.usergroups',
-    'oioioi.problemsharing',
-    'oioioi.usercontests',
-) + INSTALLED_APPS
-
 TEMPLATES[0]['OPTIONS']['context_processors'] += [
-    'oioioi.contestlogo.processors.logo_processor',
-    'oioioi.contestlogo.processors.icon_processor',
-    'oioioi.globalmessage.processors.global_message_processor',
     'oioioi.portals.processors.portal_processor',
 ]
 
 AUTHENTICATION_BACKENDS += (
     'oioioi.base.tests.IgnorePasswordAuthBackend',
     'oioioi.teachers.auth.TeacherAuthBackend',
-    'oioioi.usercontests.auth.UserContestAuthBackend',
 )
 
 MIDDLEWARE += ('oioioi.base.tests.FakeTimeMiddleware',)
@@ -80,19 +47,8 @@ MOCK_RANKINGSD = True
 
 SECRET_KEY = 'no_secret'
 OISUBMIT_MAGICKEY = 'abcdef'
-CELERY_ALWAYS_EAGER = True
-SIOWORKERS_BACKEND = 'oioioi.sioworkers.backends.LocalBackend'
-FILETRACKER_CLIENT_FACTORY = 'filetracker.client.dummy.DummyClient'
-FILETRACKER_URL = None
 USE_UNSAFE_EXEC = True
 USE_UNSAFE_CHECKER = True
-
-AVAILABLE_COMPILERS = SYSTEM_COMPILERS
-DEFAULT_COMPILERS = SYSTEM_DEFAULT_COMPILERS
-
-USE_SINOLPACK_MAKEFILES = True
-SINOLPACK_RESTRICT_HTML = False
-
 
 COMPLAINTS_EMAIL = 'dummy@example.com'
 COMPLAINTS_SUBJECT_PREFIX = '[oioioi-complaints] '
@@ -107,15 +63,11 @@ ZEUS_INSTANCES = {
 
 CACHES = {'default': {'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'}}
 
-CONFIG_VERSION = INSTALLATION_CONFIG_VERSION
-
-STATIC_ROOT = ''
-
 # Do not print migrations DEBUG to console.
 LOGGING['loggers']['django.db.backends.schema'] = {
     'handlers': ['console'],
     'level': 'INFO',
 }
 
-DEBUG = True
 CAPTCHA_TEST_MODE = True
+DEBUG = True
