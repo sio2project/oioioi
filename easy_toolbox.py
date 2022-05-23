03dd0616fa0d8d7b57c94abb79f4105aef4f50d2
Aleksander Tudruj <aleksanderwt@gmail.com>
2022-05-23 12:20:39 +0200

    Working toolbox

--- easy_toolbox.py
+++ easy_toolbox.py
@@ -14,35 +14,35 @@
 # copy-pasting commands from GitHub. This script
 # is prepared and should be upgraded or/and extended
 # for any future needs.
-
+inquirer
 import sys
 import os
-from tokenize import Number
 import inquirer
 
 
-BASE_DOCKER_COMMAND="OIOIOI_UID=$(id -u) docker-compose" + \
-     " -f docker-compose-dev.yml" + \
-     " -f extra/docker/docker-compose-dev-noserver.yml"
-
+BASE_DOCKER_COMMAND = "OIOIOI_UID=$(id -u) docker-compose" + \
+                      " -f docker-compose-dev.yml" + \
+                      " -f extra/docker/docker-compose-dev-noserver.yml"
 
 RAW_COMMANDS = [
     ("build", "Build whole OIOIOI from source.", "build", True),
     ("up", "Run all SIO2 containers", "up -d"),
-    ("down", "Stop all SIO2 containers", "down"),
-    ("run", "Run server", "exec web python3 manage.py runserver 0.0.0.0:8000"),
+    ("down", "Stop all SIO2 containers", "down", True),
+    ("run", "Run server", "exec -T web python3 manage.py runserver 0.0.0.0:8000"),
     ("bash", "Open command prompt on web container.", "exec web bash"),
-    ("bash_db", "Open command prompt on database container.", "exec db bash"),
+    ("bash_db", "Open command prompt on database container.", "exec -T db bash"),
     # This one CLEARS the database. Use wisely.
-    ("flush-db", "Clear database.", "exec web python manage.py flush --noinput", True),
-    ("add-superuser", "Create admin_admin.", "exec web python manage.py createsuperuser"),
-    ("test", "Run unit tests.", "exec web ../oioioi/test.sh"),
-    ("test-slow", "Run unit tests. (--runslow)", "exec web ../oioioi/test.sh --runslow"),
-    ("test-abc", "Run specific test file. (edit the toolbox)", "exec web ../oioioi/test.sh -v oioioi/problems/tests/test_task_archive.py"),
-    ("test-coverage", "Run coverage tests.", "exec 'web' ../oioioi/test.sh oioioi/problems --cov-report term --cov-report xml:coverage.xml --cov=oioioi"),
-    ("server-cypress", "Run CyPress test server.", "exec web python manage.py testserver ../oioioi/oioioi_cypress/cypress/fixtures/admin_admin.json --no-input --addrport 0.0.0.0:8000 --settings oioioi.cypress_settings"),
+    ("flush-db", "Clear database.", "exec -T web python manage.py flush --noinput", True),
+    ("add-superuser", "Create admin_admin.", "exec -T web python manage.py createsuperuser"),
+    ("test", "Run unit tests.", "exec -T web ../oioioi/test.sh"),
+    ("test-slow", "Run unit tests. (--runslow)", "exec -T web ../oioioi/test.sh --runslow"),
+    ("test-abc", "Run specific test file. (edit the toolbox)",
+     "exec -T web ../oioioi/test.sh -v oioioi/problems/tests/test_task_archive.py"),
+    ("test-coverage", "Run coverage tests.",
+     "exec -T 'web' ../oioioi/test.sh oioioi/problems --cov-report term --cov-report xml:coverage.xml --cov=oioioi"),
+    ("cypress-apply-settings", "Apply settings for CyPress.",
+     "exec -T web bash -c \"echo CAPTCHA_TEST_MODE=True >> settings.py\""),
 ]
-
 
 longest_command_arg = max([len(command[0]) for command in RAW_COMMANDS])
 
@@ -52,12 +52,12 @@ class Help(Exception):
 
 
 class Option:
-    def __init__(self, arg, help, command, warn = False):
-        self.arg = arg
-        self.help = help
-        self.command = command
-        self.warn = warn
-    
+    def __init__(self, _arg, _help, _command, _warn=False):
+        self.arg = _arg
+        self.help = _help
+        self.command = _command
+        self.warn = _warn
+
     def long_str(self) -> str:
         return f"Option({self.arg}, Description='{self.help}', Command='{self.command}')"
 
@@ -74,21 +74,32 @@ def check_commands() -> None:
         raise Exception("Error in COMMANDS. Same name was declared for more then one command.")
 
 
+NO_INPUT = False
+
+
 def get_action_from_args() -> Option:
-    if len(sys.argv) < 2:
+    no_flag = []
+
+    for arg in sys.argv[1:]:
+        if arg in ['--help', '-h']:
+            raise Help()
+        elif arg in ['--no-input', '-i']:
+            global NO_INPUT
+            NO_INPUT = True
+        else:
+            no_flag.append(arg)
+
+    if len(no_flag) < 1:
         return None
-    if len(sys.argv) > 2:
+    if len(no_flag) > 1:
         raise Exception("Too many arguments!")
 
-    if (sys.argv[1] in ['--help' ,'-h']):
-        raise Help()
-    
-    candidates = list(filter(lambda opt: opt.arg == sys.argv[1], COMMANDS))
+    candidates = list(filter(lambda opt: opt.arg == no_flag[0], COMMANDS))
     if len(candidates) < 1:
         raise Exception("No argument was found!")
     if len(candidates) > 1:
         raise Exception("More then one matching argument was found!")
-    
+
     return candidates[0]
 
 
@@ -106,13 +117,14 @@ def get_action_from_cli() -> Option:
 
 def run_command(command) -> None:
     print('Running command', command)
-    width = os.get_terminal_size().columns
-    print('=' * width)
-    os.system(command)
+    if not NO_INPUT:
+        width = os.get_terminal_size().columns
+        print('=' * width)
+    sys.exit(os.WEXITSTATUS(os.system(command)))
 
 
-def warn_user(action: Option) -> None:
-    print("You are going to execute command marked as `dangerous`. Are you sure? [y/N]")
+def warn_user(action: Option) -> bool:
+    print(f"You are going to exec -Tute command [{action.command}] marked as `dangerous`. Are you sure? [y/N]")
     while True:
         choice = input().lower()
         if len(choice) == 0 or "no"[:len(choice)] == choice:
@@ -125,7 +137,7 @@ def warn_user(action: Option) -> None:
 
 def run() -> None:
     action = get_action_from_args() or get_action_from_cli()
-    if action.warn:
+    if action.warn and not NO_INPUT:
         if not warn_user(action):
             print("Aborting.")
             return
@@ -134,9 +146,9 @@ def run() -> None:
 
 def print_help() -> None:
     print("OIOIOI helper toolbox", "", "This script allows to control OIOIOI with Docker commands.",
-            f"Commands are always being run with '{BASE_DOCKER_COMMAND}' prefix.",
-            f"Aveliable commands are: ", "",
-            *COMMANDS, "", "Example `build`:", f"{sys.argv[0]} build", sep="\n")
+          f"Commands are always being run with '{BASE_DOCKER_COMMAND}' prefix.",
+          f"Aveliable commands are: ", "",
+          *COMMANDS, "", "Example `build`:", f"{sys.argv[0]} build", sep="\n")
 
 
 def main() -> None:
