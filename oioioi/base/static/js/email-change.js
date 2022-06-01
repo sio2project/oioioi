753a7b6504b349f08076807b959426f0d7effabd
Kacper Lewandowski <kl429158@students.mimuw.edu.pl>
2022-06-01 18:59:31 +0200

    added semicolons in js

--- oioioi/base/static/js/email-change.js
+++ oioioi/base/static/js/email-change.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-    let password_form = document.getElementById('id_confirm_password').parentElement
+    let password_form = document.getElementById('id_confirm_password').parentElement;
     if (!password_form.classList.contains('has-error'))
-        password_form.classList.add('hidden')
+        password_form.classList.add('hidden');
     $("#id_email").change(function () {
-        password_form.classList.remove('hidden')
-    })
+        password_form.classList.remove('hidden');
+    });
 });
