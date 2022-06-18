e3e7f018bf169d8e37c9165443980a4b1ec18bbb
27klew <67709892+27klew@users.noreply.github.com>
2022-06-18 22:12:51 +0200

    update email-change.js
    Updated password field handler to recognize new error markers.
    This prevents the script from hiding "password incorrect" prompt.

--- oioioi/base/static/js/email-change.js
+++ oioioi/base/static/js/email-change.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
-    let password_form = document.getElementById('id_confirm_password').parentElement;
-    if (!password_form.classList.contains('has-error'))
-        password_form.classList.add('d-none');
+    let password_form_control = document.getElementById('id_confirm_password');
+    let password_form_group = password_form_control.parentElement;
+
+    if (!password_form_control.classList.contains('is-invalid'))
+        password_form_group.classList.add('d-none');
     $("#id_email").change(function () {
-        password_form.classList.remove('d-none');
+        password_form_group.classList.remove('d-none');
     });
 });
