a4c9ad87906a03e2c3181c99a1e6f32862dc2358
andr729 <67708826+andr729@users.noreply.github.com>
2022-06-20 13:52:54 +0200

    SIO-2486 Hotfixes from szkopul 2  (#75)
    * Add decoding to HTML problem statements.
    
    * HOTFIX: Teachers & forum
    
    * Add a command for gathering submissions statistics.
    
    * Add command for finding SE submissions.
    
    Co-authored-by: Alicja Kluczek <alicja2602@gmail.com>
    Co-authored-by: sio2 <sio2@sio2project.mimuw.edu.pl>
    Co-authored-by: sio2 <sio2@snag.dasie.mimuw.edu.pl>

---
+++ oioioi/contests/management/commands/find_all_se_submissions.py
@@ -0,0 +1,45 @@
+from __future__ import print_function
+
+import six
+import time
+
+from django.core.management.base import BaseCommand
+from django.db.models import Q
+from django.utils.translation import ugettext as _
+from django.db import transaction
+from oioioi.contests.models import Contest, Round, Submission, SubmissionReport
+from datetime import datetime, timedelta
+
+class Command(BaseCommand):
+    help = _("Display all submissions that got System Error in the past N days")
+
+    def add_arguments(self, parser):
+        parser.add_argument('-n', '--numdays',
+                            action='store',
+                            type=int,
+                            dest='numdays',
+                            help="How many past days of reports to check",
+                            default=10)
+
+    def handle(self, *args, **options):
+        numdays = options["numdays"]
+        subs = Submission.objects.filter(date__gte=datetime.now()-timedelta(days=numdays))
+        raporty = SubmissionReport.objects.filter(submission__in=subs)
+        # problem_instance.controller.judge(submission, is_rejudge=True)
+        q = Q(
+            failurereport__isnull=False,
+        ) | Q(
+            testreport__status='SE'
+        )
+        raporty_z_se = raporty.filter(q).distinct()
+        for r in raporty_z_se:
+            pi = r.submission.problem_instance
+
+            with transaction.atomic():
+                pi.controller.judge(r.submission, is_rejudge=True)
+            time.sleep(1)
+            print(r.submission.id, end=",")
+            print(r.submission, end=",")
+            print(pi.contest, ",",  pi.problem, ",", pi.short_name)
+
+
