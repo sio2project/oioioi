0cd959957c800b7d8ce0fd938f992a22e590a3d6
pixl.dt <olaf.targowski@otsrv.net>
2022-11-20 21:45:58 +0100

    Fix using WORKER_CONCURRENCY

--- worker_init.sh
+++ worker_init.sh
@@ -11,11 +11,11 @@ mkdir -pv /sio2/deployment/logs/database
 
 echo "LOG: Launching worker at `hostname`"
 export FILETRACKER_URL="http://web:9999"
-[ -z $WORKER_CONCURRENCY ] && WORKER_CONCURRENCY=2
+[ -n "$WORKER_CONCURRENCY" ] || WORKER_CONCURRENCY=2
 
 exec python3 $(which twistd) --nodaemon --pidfile=/home/oioioi/worker.pid \
         -l /sio2/deployment/logs/worker`hostname`.log worker \
         --can-run-cpu-exec \
-        -n worker`hostname` -c ${WORKER_CONRURRENCY} web \
+        -n worker`hostname` -c $WORKER_CONRURRENCY web \
         > /sio2/deployment/logs/twistd_worker.out \
         2> /sio2/deployment/logs/twistd_worker.err
--- worker_init_prod.sh
+++ worker_init_prod.sh
@@ -12,10 +12,10 @@ mkdir -pv /sio2/logs/{supervisor,runserver,database}
 echo "LOG: Launching worker at `hostname`"
 export FILETRACKER_URL="http://web:9999"
 rm -f /home/oioioi/worker.pid
-[ -z $WORKER_CONCURRENCY ] && WORKER_CONCURRENCY=2
+[ -n "$WORKER_CONCURRENCY" ] || WORKER_CONCURRENCY=2
 
 exec twistd --nodaemon --pidfile=/home/oioioi/worker.pid \
         -l /sio2/logs/worker`hostname`.log worker \
-        -n worker`hostname` -c ${WORKER_CONRURRENCY} web \
+        -n worker`hostname` -c $WORKER_CONCURRENCY web \
         > /sio2/logs/twistd_worker.out #\
         #2> /sio2/logs/twistd_worker.err
