16ae046372b7e17522c9981705db1757f138e050
pixl.dt <olaf.targowski@otsrv.net>
2022-12-21 20:12:50 +0100

    Clean up supervision info in navbar

--- oioioi/supervision/processors.py
+++ oioioi/supervision/processors.py
@@ -1,10 +1,13 @@
+from django.template.loader import render_to_string
 from oioioi.supervision.utils import is_user_under_supervision
 
 
 def under_supervision(request):
+    is_under_supervision = False
     if hasattr(request, 'user'):
-        return {
-            'is_under_supervision': is_user_under_supervision(getattr(request, 'user'))
-        }
-    else:
-        return {'is_under_supervision': False}
+        is_under_supervision=is_user_under_supervision(getattr(request, 'user'))
+    return {
+        'is_under_supervision': is_under_supervision,
+        'extra_navbar_right_supervision': render_to_string('supervision/navbar-user-supervision.html',
+                                                           dict(is_under_supervision=is_under_supervision)),
+    }
