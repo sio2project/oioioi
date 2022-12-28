5fbf34de036e5bf260e865cbfd9e7eab562f6993
pixl.dt <olaf.targowski@otsrv.net>
2022-12-28 15:24:30 +0100

    Fixup of phase and a minor missing migration

--- oioioi/phase/admin.py
+++ oioioi/phase/admin.py
@@ -60,7 +60,7 @@ class PhaseAdmin(admin.ModelAdmin):
 
 @make_request_condition
 def is_phase_contest(request):
-    return hasattr(request.contest, 'controller') and request.contest.controller.is_phase_contest
+    return hasattr(request.contest, 'controller') and hasattr(request.contest.controller, 'is_phase_contest') and request.contest.controller.is_phase_contest
 
 
 contest_site.contest_register(Phase, PhaseAdmin)
