6df082d520664fa911b0318d11f8b6e7413088d5
Stanisław Czech <stasio.czech@gmail.com>
2023-02-05 18:58:43 +0100

    ranking hotfix

--- oioioi/supervision/processors.py
+++ oioioi/supervision/processors.py
@@ -2,9 +2,10 @@ from django.template.loader import render_to_string
 
 
 def supervision_processor(request):
+    is_under_supervision=request.is_under_supervision if hasattr(request, 'is_under_supervision') else False
     return {
-        'is_under_supervision': request.is_under_supervision,
+        'is_under_supervision': is_under_supervision,
         'extra_navbar_right_supervision': render_to_string(
             'supervision/navbar-user-supervision.html',
-            dict(is_under_supervision=request.is_under_supervision)),
+            dict(is_under_supervision=is_under_supervision)),
     }
