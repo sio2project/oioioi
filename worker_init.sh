d0a179c992eb280f598c41a5442d8e96850c5040
talent.oboz <me@otsrv.net>
2023-03-16 16:20:46 +0100

    Setup deployment for discrete nginx

--- worker_init.sh
+++ worker_init.sh
@@ -4,9 +4,6 @@ set -x
 
 sudo apt install -y proot
 
-/sio2/oioioi/wait-for-it.sh -t 60 "db:5432"
-/sio2/oioioi/wait-for-it.sh -t 0  "web:8000"
-
 mkdir -pv /sio2/deployment/logs/database
 
 [ -n "$WORKER_CONCURRENCY" ] || WORKER_CONCURRENCY=$(nproc)
@@ -14,11 +11,16 @@ mkdir -pv /sio2/deployment/logs/database
 
 echo "LOG: Launching worker at `hostname`"
 export FILETRACKER_URL="http://web:9999"
+
+/sio2/oioioi/wait-for-it.sh -t 60 "db:5432"
+/sio2/oioioi/wait-for-it.sh -t 0  "web:9999"
+
 exec twistd --nodaemon --pidfile=/home/oioioi/worker.pid \
         -l /sio2/deployment/logs/worker`hostname`.log worker \
         --can-run-cpu-exec \
         -n worker`hostname` \
         -c $WORKER_CONCURRENCY \
-        -r $WORKER_RAM_MB web \
+        -r $WORKER_RAM_MB \
+	web \
         > /sio2/deployment/logs/twistd_worker.out \
         #2> /sio2/deployment/logs/twistd_worker.err
