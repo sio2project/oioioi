9f6ca54499ab41b275430e9b12691a8f8649f60c
etiaro <j.klimek2002@gmail.com>
2023-03-22 22:05:06 +0100

    moved database config to env variables

--- oioioi/default_settings.py
+++ oioioi/default_settings.py
@@ -37,6 +37,18 @@ PUBLIC_ROOT_URL = 'http://localhost'
 # None - nothing will be run
 SERVER = None
 
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get('OIOIOI_DB_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.environ.get('OIOIOI_DB_NAME'),           # Or path to database file if using sqlite3.
+        'USER': os.environ.get('OIOIOI_DB_USER'),           # Not used with sqlite3.
+        'PASSWORD': os.environ.get('OIOIOI_DB_PASSWORD'),   # Not used with sqlite3.
+        'HOST': os.environ.get('OIOIOI_DB_HOST'),           # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': os.environ.get('OIOIOI_DB_PORT'),           # Set to empty string for default. Not used with sqlite3.
+        'ATOMIC_REQUESTS': True,         # Don't touch unless you know what you're doing.
+    }
+}
+
 # Controls if uwsgi in default configuration shall use gevent loop.
 # To use it, you have to install gevent - please consult
 # https://github.com/surfly/gevent
