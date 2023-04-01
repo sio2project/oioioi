6e47adb7f81a9fc3a83a36d387fdf360051d1be6
A-dead-pixel <me@otsrv.net>
2023-04-01 16:36:38 +0200

    Fix supervision checks for non-existent rounds

--- oioioi/supervision/utils.py
+++ oioioi/supervision/utils.py
@@ -6,6 +6,7 @@ def can_user_enter_round(request_or_context, round):
     if not hasattr(request_or_context, 'is_under_supervision') or \
             request_or_context.user.is_superuser:
         return True
+    id = getattr(round, 'id', None)
     if request_or_context.is_under_supervision:
-        return round.id in request_or_context.supervision_visible_rounds
-    return round.id not in request_or_context.supervision_hidden_rounds
+        return id in request_or_context.supervision_visible_rounds
+    return id not in request_or_context.supervision_hidden_rounds
