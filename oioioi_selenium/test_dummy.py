8f59544087f0652d4c85d18afc4d07d6aa148971
A-dead-pixel <me@otsrv.net>
2023-07-09 16:08:49 +0200

    Remove selenium and easy_install
    Also update tox.ini

--- oioioi_selenium/test_dummy.py
+++
@@ -1,8 +0,0 @@
-from . import OIOIOISeleniumTestCase
-
-
-class TestDummy(OIOIOISeleniumTestCase):
-    def test_dummy(self):
-        driver = self.driver
-        driver.get("/")
-        driver.save_screenshot("test_dummy.png")
