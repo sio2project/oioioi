ae8fe2203bff6f9cbdda998919b23acd932d28c0
A-dead-pixel <me@otsrv.net>
2023-07-17 17:21:04 +0200

    Properly handle the OIOIOI_TIMEZONE env var

--- oioioi/default_settings.py
+++ oioioi/default_settings.py
@@ -74,11 +74,9 @@ STATEMENT_LANGUAGES = (
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = os.getenv('OIOIOI_TIMEZONE', None)
+TIME_ZONE = os.getenv('OIOIOI_TIMEZONE', 'UTC')
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
