99478ec4b352befd5c227524729689e6dc3828ce
A-dead-pixel <me@otsrv.net>
2023-09-03 15:22:23 +0200

    Fix preparing value in user selection form

--- oioioi/base/utils/user_selection.py
+++ oioioi/base/utils/user_selection.py
@@ -154,6 +154,11 @@ class UserSelectionField(forms.CharField):
     def prepare_value(self, value):
         if isinstance(value, User):
             return value.username
+        if isinstance(value, int):
+            try:
+                return User.objects.get(id=value).username
+            except User.DoesNotExist:
+                pass
         return super(UserSelectionField, self).prepare_value(value)
 
     def to_python(self, value):
