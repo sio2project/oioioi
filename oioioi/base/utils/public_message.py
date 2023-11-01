cd9dad015c090d03b3d18260837dd004bdc45b47
Mateusz Masiarz <matimasiarz@gmail.com>
2023-11-01 19:40:05 +0100

    Small refactor

--- oioioi/base/utils/public_message.py
+++ oioioi/base/utils/public_message.py
@@ -1,8 +1,8 @@
-def get_public_message(request, model, controller_default_value=None):
-    if controller_default_value is None:
-        controller_default_value = ''
+def get_public_message(request, model, controller_default_attribute=None):
+    if controller_default_attribute is None:
+        controller_default_attribute = ''
     contest = request.contest
     if not model.objects.filter(contest=contest).exists():
-        content = getattr(contest.controller, controller_default_value, '')
+        content = getattr(contest.controller, controller_default_attribute, '')
         model.objects.create(contest=contest, content=content)
     return model.objects.get(contest=contest)
