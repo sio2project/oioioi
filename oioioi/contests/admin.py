f2dabcecea250459b44b9b7c6ecb3cd9735dc7a0
tkwiatkowski <t.kwiatkowski@fri.edu.pl>
2024-01-10 00:03:56 +0100

    Make read-only fields in ContestAdmin

--- oioioi/contests/admin.py
+++ oioioi/contests/admin.py
@@ -235,6 +235,10 @@ class ContestAdmin(admin.ModelAdmin):
 
     def get_readonly_fields(self, request, obj=None):
         if obj:
+            if obj.is_archived:
+                return list(self.readonly_fields) + \
+                    [field.name for field in obj._meta.fields] + \
+                    [field.name for field in obj._meta.many_to_many]
             return self.readonly_fields + ['id', 'controller_name']
         return []
 
