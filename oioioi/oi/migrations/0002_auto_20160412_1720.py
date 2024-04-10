bc9aa648334127f06566b3f5bf38074f585e3ac0
Dawid Ratyński <dr448571@students.mimuw.edu.pl>
2024-04-10 17:49:13 +0200

    Fix broken migration in OI app

--- oioioi/oi/migrations/0002_auto_20160412_1720.py
+++ oioioi/oi/migrations/0002_auto_20160412_1720.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+import oioioi.participants.fields
 
 
 class Migration(migrations.Migration):
@@ -16,6 +17,16 @@ class Migration(migrations.Migration):
             name='region',
             field=models.IntegerField(verbose_name='region', null=True),
         ),
+        migrations.AlterField(
+            model_name='oionsiteregistration',
+            name='participant',
+            field=oioioi.participants.fields.OneToOneBothHandsCascadingParticipantField(related_name='oi_oionsiteregistration', to='participants.Participant', on_delete=models.CASCADE, null=True),
+        ),
+        migrations.AlterField(
+            model_name='region',
+            name='contest',
+            field=models.ForeignKey(to='contests.Contest', on_delete=models.CASCADE, null=True),
+        )
     ]
 
     state_operations = [
