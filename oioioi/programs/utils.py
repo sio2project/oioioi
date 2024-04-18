c425ba3d1c65dab40588b3cc5c1db143205ee356
Mateusz Masiarz <matimasiarz@gmail.com>
2024-04-18 23:08:57 +0200

    Fix bug in calculating the percentage

--- oioioi/programs/utils.py
+++ oioioi/programs/utils.py
@@ -100,7 +100,7 @@ def min_group_scorer(test_results):
 def discrete_test_scorer(test, result):
     status = result['result_code']
     percentage = result.get('result_percentage', (100, 1))
-    max_score = ceil(Fraction(*percentage) * test['max_score'])
+    max_score = ceil(Fraction(*percentage) / 100. * test['max_score'])
     score = max_score if status == 'OK' else 0
     return IntegerScore(score), IntegerScore(test['max_score']), status
 
@@ -111,7 +111,7 @@ def threshold_linear_test_scorer(test, result):
     used = result.get('time_used', 0)
     status = result['result_code']
     percentage = result.get('result_percentage', (100, 1))
-    max_score = ceil(Fraction(*percentage) * test['max_score'])
+    max_score = ceil(Fraction(*percentage) / 100. * test['max_score'])
     test_max_score = IntegerScore(test['max_score'])
 
     if status != 'OK':
