a04c578a982c66bf31affd47a61e19162bb3a186
Mateusz Jacniacki <mateuszjacniacki2@gmail.com>
2024-05-07 14:12:16 +0200

    Refactor contest monitoring view

--- oioioi/statistics/views.py
+++ oioioi/statistics/views.py
@@ -1,6 +1,4 @@
 from collections import defaultdict
-from datetime import datetime
-from pprint import pprint
 
 from django.core.exceptions import PermissionDenied
 from django.db.models import Count
@@ -8,7 +6,6 @@ from django.http import Http404
 from django.template.response import TemplateResponse
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
-from pytz import UTC
 
 from oioioi.base.menu import menu_registry
 from oioioi.base.permissions import enforce_condition
@@ -142,55 +139,86 @@ def statistics_view(
     contest_exists & can_enter_contest & can_see_stats
 )
 def monitoring_view(request):
-    cur_time = request.timestamp
-    r_times = []
+    q_size = QueuedJob.objects.filter(submission__problem_instance__contest=request.contest).count()
+    q_size_global = QueuedJob.objects.count()
+    sys_error_count = (
+        SubmissionReport.objects.filter(status='ACTIVE', failurereport__isnull=False).count()
+        + SubmissionReport.objects.filter(status='ACTIVE', testreport__status='SE').count()
+    )
+
+    unanswered_questions = (Message.objects.filter(kind='QUESTION', message=None, contest=request.contest).count())
+    oldest_unanswered_question = (Message.objects.filter(kind='QUESTION', message=None, contest=request.contest)
+                                  .order_by('pub_date').first())
+    oldest_unanswered_question_date = oldest_unanswered_question.date if oldest_unanswered_question else None
+
+    submissions_info = (Submission.objects.filter(problem_instance__contest=request.contest).values('kind')
+                        .annotate(total=Count('kind')).order_by())
+    rounds_info = get_rounds_info(request)
+    permissions_info = get_permissions_info(request)
+    attachments_info = get_attachments_info(request)
+    tests_info = get_tests_info(request)
+    return TemplateResponse(
+        request,
+        'statistics/monitoring.html',
+        {
+            'title': _("Monitoring"),
+            'rounds_times': rounds_info,
+            'permissions_info': permissions_info,
+            'q_size': q_size,
+            'q_size_global': q_size_global,
+            'attachments': attachments_info,
+            'unanswered_questions': unanswered_questions,
+            'oldest_unanswered_question': oldest_unanswered_question_date,
+            'submissions_info': submissions_info,
+            'tests_info': tests_info,
+            'sys_error_count': sys_error_count,
+        },
+    )
+
+
+def get_rounds_info(request):
+    rounds_info = []
     for round_, rt in rounds_times(request, request.contest).items():
-        round_time_info = {'name': str(round_)}
-        round_time_info['start'] = rt.start or _("Not set")
+        round_time_info = {'name': str(round_), 'start': rt.start or _("Not set")}
         if rt.start:
-            round_time_info['start_relative'] = str(rt.start - cur_time)[:-7] if rt.is_future(cur_time) else _("Started")
+            round_time_info['start_relative'] = str(rt.start - request.timestamp)[:-7] if rt.is_future(
+                request.timestamp) else _("Started")
         else:
             round_time_info['start_relative'] = _("Not set")
         round_time_info['end'] = rt.end or _("Not set")
         if rt.end:
-            round_time_info['end_relative'] = str(rt.end - cur_time)[:-7] if not rt.is_past(cur_time) else _("Finished")
+            round_time_info['end_relative'] = str(rt.end - request.timestamp)[:-7] if not rt.is_past(
+                request.timestamp) else _("Finished")
         else:
             round_time_info['end_relative'] = _("Not set")
-        r_times.append(round_time_info)
+        rounds_info.append(round_time_info)
+    return rounds_info
 
-    permissions_count = {
+
+def get_attachments_info(request):
+    attachments = ContestAttachment.objects.filter(contest_id=request.contest.id).order_by('id')
+    for attachment in attachments:
+        pub_date_relative = None
+        if attachment.pub_date:
+            pub_date_relative = str(attachment.pub_date - request.timestamp)[:-7] \
+                if attachment.pub_date > request.timestamp else _("Published")
+        setattr(attachment, 'pub_date_relative', pub_date_relative)
+    return attachments
+
+
+def get_permissions_info(request):
+    permissions_info = {
         permission_name: (ContestPermission
                           .objects
                           .filter(contest_id=request.contest.id, permission=permission_cls)
                           .count())
         for permission_cls, permission_name in contest_permissions
     }
-    permissions_count['Participant'] = Participant.objects.filter(contest_id=request.contest.id).count()
-    q_size = (QueuedJob.objects
-                .filter(submission__problem_instance__contest=request.contest)
-                .count())
-    q_size_global = (QueuedJob.objects
-                .count())
+    permissions_info['Participant'] = Participant.objects.filter(contest_id=request.contest.id).count()
+    return permissions_info
 
-    sys_error_count = (
-        SubmissionReport.objects.filter( status='ACTIVE', failurereport__isnull=False).count()
-        +
-        SubmissionReport.objects.filter( status='ACTIVE', testreport__status='SE').count()
-    )
 
-    attachments = ContestAttachment.objects.filter(contest_id=request.contest.id).order_by('id')
-    for attachment in attachments:
-        pub_date_relative = None
-        if attachment.pub_date:
-            pub_date_relative = str(attachment.pub_date - cur_time)[:-7] if attachment.pub_date > cur_time else _("Published")
-        setattr(attachment, 'pub_date_relative', pub_date_relative)
-    unanswered_questions = (Message.objects.filter(kind='QUESTION', message=None, contest=request.contest).count())
-    oldest_unanswered_question = (Message.objects.filter(kind='QUESTION', message=None, contest=request.contest)
-                                  .order_by('pub_date').first())
-    oldest_unanswered_question_date = oldest_unanswered_question.date if oldest_unanswered_question else None
-
-    submissions_info = Submission.objects.filter(problem_instance__contest=request.contest).values('kind').annotate(total=Count('kind')).order_by()
-
+def get_tests_info(request):
     tests_info = defaultdict(lambda: defaultdict(lambda: {
         'problem_name': None,
         'testrun_config': None,
@@ -216,22 +244,4 @@ def monitoring_view(request):
         tests_info[trc.problem_instance.round][trc.problem_instance.id]['testrun_config'] = trc
 
     tests_info = dict({r: dict(t) for r, t in tests_info.items()})
-
-    return TemplateResponse(
-        request,
-        'statistics/monitoring.html',
-        {
-            'title': _("Monitoring"),
-            'rounds_times': r_times,
-            'permissions_count': permissions_count,
-            'links': links(request),
-            'q_size': q_size,
-            'q_size_global': q_size_global,
-            'attachments': attachments,
-            'unanswered_questions': unanswered_questions,
-            'oldest_unanswered_question': oldest_unanswered_question_date,
-            'submissions_info': submissions_info,
-            'tests_info': tests_info,
-            'sys_error_count': sys_error_count,
-        },
-    )
+    return tests_info
