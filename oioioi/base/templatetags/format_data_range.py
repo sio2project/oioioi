e54877e332b63e3063144fa66b9762dc5826f449
Piotr Plywacz <piotrus.plywacz@gmail.com>
2024-05-13 18:06:03 +0200

    Add date translation

--- oioioi/base/templatetags/format_data_range.py
+++ oioioi/base/templatetags/format_data_range.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils import formats
 
 register = template.Library()
 
@@ -6,15 +7,15 @@ register = template.Library()
 def format_data_range(start, end):
     if start is not None and end is not None:
         if start.year == end.year and start.month == end.month and start.day == end.day:
-            day = start.strftime("%d %b %Y")
-            start_str = start.strftime("%H:%M")
-            end_str = end.strftime("%H:%M")
+            day = formats.date_format(start, "j E Y")
+            start_str = formats.date_format(start, "H:i")
+            end_str = formats.date_format(end, "H:i")
             return '(' + day + ', ' + start_str + ' - ' + end_str + ')'
 
     start_str = ''
     if start is not None:
-        start_str = start.strftime("%d %b %Y, %H:%M")
+        start_str = formats.date_format(start, "j E Y, H:i")
     end_str = ''
     if end is not None:
-        end_str = end.strftime("%d %b %Y, %H:%M")
+        end_str = formats.date_format(end, "j E Y, H:i")
     return '(' + start_str + ' - ' + end_str + ')'
--- oioioi/contests/tests/tests.py
+++ oioioi/contests/tests/tests.py
@@ -774,6 +774,7 @@ class TestMySubmissions(TestCase):
         self.assertEqual(len(ini_ok), 1)
         self.assertEqual(len(ini_err), 1)
 
+from django.utils.translation import activate
 
 class TestManyRounds(TestsUtilsMixin, TestCase):
     fixtures = [
@@ -948,10 +949,23 @@ class TestManyRounds(TestsUtilsMixin, TestCase):
             for user in ['test_admin', 'test_contest_admin', 'test_user', 'test_observer']:
                 self.assertTrue(self.client.login(username=user))
                 response = self.client.get(url)
-                self.assertContains(response, "(31 Jul 2011, 20:27 - )")
-                self.assertContains(response, "(31 Jul 2012, 20:27 - 21:27)")
-                self.assertContains(response, "(30 Jul 2012, 20:27 - 31 Jul 2012, 21:27)")
-                
+                self.assertContains(response, "(31 July 2011, 20:27 - )")
+                self.assertContains(response, "(31 July 2012, 20:27 - 21:27)")
+                self.assertContains(response, "(30 July 2012, 20:27 - 31 July 2012, 21:27)")
+
+    def test_polish_round_dates(self):
+        self.client.cookies['lang'] = 'pl'
+        contest = Contest.objects.get()
+        url = reverse('problems_list', kwargs={'contest_id': contest.id})
+        with fake_time(datetime(2024, 1, 1, tzinfo=timezone.utc)):
+            for user in ['test_admin', 'test_contest_admin', 'test_user', 'test_observer']:
+                self.assertTrue(self.client.login(username=user))
+                response = self.client.get(url)
+                self.assertContains(response, "(31 lipca 2011, 20:27 - )")
+                self.assertContains(response, "(31 lipca 2012, 20:27 - 21:27)")
+                self.assertContains(response, "(30 lipca 2012, 20:27 - 31 lipca 2012, 21:27)")
+        self.client.cookies['lang'] = 'en'
+
 
 class TestMultilingualStatements(TestCase, TestStreamingMixin):
     fixtures = [
