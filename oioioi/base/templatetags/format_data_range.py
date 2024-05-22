09daf7960f53d8e6a8430b215caa0bfdf84d12a2
Piotr Plywacz <piotrus.plywacz@gmail.com>
2024-05-22 20:19:46 +0200

    Code cleanup

--- oioioi/base/templatetags/format_data_range.py
+++ oioioi/base/templatetags/format_data_range.py
@@ -6,10 +6,9 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def format_data_range(context, start, end):
+    start = template_localtime(start, context.use_tz) if start is not None else None
+    end = template_localtime(end, context.use_tz) if end is not None else None
     if start is not None and end is not None:
-        start = template_localtime(start, context.use_tz)
-        end = template_localtime(end, context.use_tz)
-
         if start.year == end.year and start.month == end.month and start.day == end.day:
             day = formats.date_format(start, "j E Y")
             start_str = formats.date_format(start, "H:i")
@@ -18,10 +17,8 @@ def format_data_range(context, start, end):
 
     start_str = ''
     if start is not None:
-        start = template_localtime(start, context.use_tz)
         start_str = formats.date_format(start, "j E Y, H:i")
     end_str = ''
     if end is not None:
-        end = template_localtime(end, context.use_tz)
         end_str = formats.date_format(end, "j E Y, H:i")
     return '(' + start_str + ' - ' + end_str + ')'
