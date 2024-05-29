b909df44ba293df9e0d149eed3f4652e94469adc
Mateusz Jacniacki <mateuszjacniacki2@gmail.com>
2024-05-29 17:13:18 +0200

    WIP

--- oioioi/statistics/tests_monitoring.py
+++ oioioi/statistics/tests_monitoring.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 
 from oioioi.base.tests import TestCase, fake_time
 from oioioi.contests.models import Contest, ProblemInstance
+from oioioi.statistics.views import get_permissions_info, get_rounds_info
 
 
 class TestContestMonitoringViews(TestCase):
@@ -20,13 +21,14 @@ class TestContestMonitoringViews(TestCase):
         'test_extra_problem',
         'test_permissions',
         'test_messages',
+        'test_second_user_messages',
     ]
 
     def setUp(self):
         self.request = RequestFactory().request()
         self.request.user = User.objects.get(username='test_user')
         self.request.contest = Contest.objects.get()
-        self.request.timestamp = datetime.now().replace(tzinfo=timezone.utc)
+        self.request.timestamp = datetime(2014, 8, 5, tzinfo=timezone.utc)
 
     def test_permissions_info(self):
         contest = Contest.objects.get()
@@ -44,13 +46,16 @@ class TestContestMonitoringViews(TestCase):
             f.write(str(response.content))
             f.close()
 
-    def test_round_times(self):
+    def test_round_info(self):
         contest = Contest.objects.get()
-        url = reverse('monitoring', kwargs={'contest_id': contest.id})
-
-        self.assertTrue(self.client.login(username='test_admin'))
         with fake_time(datetime(2015, 7, 5, tzinfo=timezone.utc)):
-            response = self.client.get(url)
-            self.assertRegex(str(response.content), r"Past round(</td>.{0,50}<td>.{0,50}){2}Started")
-            self.assertRegex(str(response.content), r"Past round(</td>.{0,50}<td>.{0,50}){4}Finished")
+            self.assertTrue(self.client.login(username='test_admin'))
+            rounds_info = get_rounds_info(self.request)
+            for ri in rounds_info:
+                if ri['name'] == 'Past round':
+                    self.assertTrue(ri['start_relative'] == 'Started')
+                    self.assertTrue(ri['end_relative'] == 'Finished')
+                if ri['name'] == 'Future round':
+                    self.assertTrue(ri['start_relative'] == '360 days, 20:27:58')
 
+    def test_attachments_info(self):
