f1d6b090cca886e6abe9536e3b97fb488d12db7a
Mateusz Masiarz <matimasiarz@gmail.com>
2024-09-07 16:00:42 +0200

    Welcome page only visible when any message exists

--- oioioi/welcomepage/urls.py
+++ oioioi/welcomepage/urls.py
@@ -7,4 +7,5 @@ app_name = 'welcomepage'
 noncontest_patterns = [
     re_path(r'^welcome/$', views.welcome_page_view, name='welcome_page'),
     re_path(r'^edit_welcome_page/$', views.edit_welcome_page_view, name='edit_welcome_page'),
+    re_path(r'^delete_welcome_page/$', views.delete_welcome_page_view, name='delete_welcome_page'),
 ]
--- oioioi/welcomepage/views.py
+++ oioioi/welcomepage/views.py
@@ -1,25 +1,26 @@
+from django.urls import reverse
 from django.conf import settings
 from django.shortcuts import redirect
 from django.template.response import TemplateResponse
 from django.utils.translation import gettext_lazy as _
 from django.utils.translation import get_language_from_request
 
+from oioioi.base.admin import system_admin_menu_registry
 from oioioi.base.main_page import register_main_page_view
 from oioioi.base.permissions import enforce_condition, is_superuser
 from oioioi.welcomepage.forms import WelcomePageMessageFormset
 from oioioi.welcomepage.models import WelcomePageMessage
+from oioioi.welcomepage.utils import any_welcome_messages
 
 
-@register_main_page_view(order=100)
+@register_main_page_view(order=110, condition=any_welcome_messages)
+@enforce_condition(any_welcome_messages)
 def welcome_page_view(request):
     current_language = get_language_from_request(request)
     try:
         welcome_page_msg = WelcomePageMessage.objects.get(language=current_language)
     except WelcomePageMessage.DoesNotExist:
-        try:
-            welcome_page_msg = WelcomePageMessage.objects.first()
-        except WelcomePageMessage.DoesNotExist:
-            welcome_page_msg = None
+        welcome_page_msg = WelcomePageMessage.objects.first()
     return TemplateResponse(
         request,
         'welcomepage/welcome-page.html',
@@ -31,6 +32,12 @@ def welcome_page_view(request):
     )
 
 
+system_admin_menu_registry.register(
+    'welcome_page',
+    _("Edit welcome page"),
+    lambda request: reverse('edit_welcome_page'),
+    order=80,
+)
 @enforce_condition(is_superuser)
 def edit_welcome_page_view(request):
     if request.method == 'POST':
@@ -62,3 +69,9 @@ def edit_welcome_page_view(request):
             'formset': formset
         },
     )
+
+
+@enforce_condition(is_superuser)
+def delete_welcome_page_view(request):
+    WelcomePageMessage.objects.all().delete()
+    return redirect(reverse('index'))
