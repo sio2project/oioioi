8117923c63134b2cea5a088871ee48d9c971f121
Grzegorz Krawczyk <grzes.krawczyk2005@gmail.com>
2025-02-22 00:12:21 +0100

    Only show the top two proposed tags for each task.

--- oioioi/problems/templatetags/tag.py
+++ oioioi/problems/templatetags/tag.py
@@ -1,7 +1,8 @@
-from django.db.models import prefetch_related_objects
+from django.db.models import Prefetch, prefetch_related_objects
 from django.template import Library
 from django.utils.html import format_html
 from oioioi.base.utils.tags import get_tag_name, get_tag_prefix
+from oioioi.problems.models import AggregatedAlgorithmTagProposal
 
 register = Library()
 
@@ -16,6 +17,15 @@ def prefetch_tags(problems):
         'origininfovalue_set__localizations',
         'origininfovalue_set__parent_tag__localizations',
     )
+    prefetch_related_objects(
+        problems,
+        Prefetch(
+            'aggregatedalgorithmtagproposal_set',
+            queryset=AggregatedAlgorithmTagProposal.objects.order_by('-amount')[:2],
+            to_attr='top_tag_proposals'
+        )
+    )
+
     return u''
 
 
