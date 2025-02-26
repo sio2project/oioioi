df6804e6077a4ead217f0813fc56c03a7df8ff69
Grzegorz Krawczyk <grzes.krawczyk2005@gmail.com>
2025-02-26 18:24:15 +0100

    Move the script controlling tag proposals to a separate file.

---
+++ oioioi/problems/static/problems/problemset/control-tag-proposals.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(){
+    var checkbox = document.getElementById("show-tag-proposals");
+    if (checkbox) {
+        var proposals = document.querySelectorAll(".aggregated-proposals");
+        function toggleProposals() {
+            proposals.forEach(function(div){
+                div.style.display = checkbox.checked ? "inline-block" : "none";
+            });
+        }
+        checkbox.addEventListener("change", toggleProposals);
+        toggleProposals(); // on page load
+    }
+});
