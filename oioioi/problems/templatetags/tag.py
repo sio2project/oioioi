8df7741bdbb16564066a53a4c02b9225af2d0dae
Grzegorz Franciszek Krawczyk <106813125+segir187@users.noreply.github.com>
2025-03-12 18:36:15 +0100

    Aggregated tag proposals in probset (#466)
    Added a way to show proposed tags in the problemset.
    
    Changes:
    * Add settings to configure whether to show proposed tags at all and details of which tags are shown.
    * Add a checkbox which toggles if the tags are shown.
    * Change spacing between elements on the problemset page to fit the new element.
    * Overhaul way of displaying AggregatedTagProposals to be better-looking.
    * New fixture taken from the setUp function of PopulateAggregatedTagProposalsTest in test_data_migrations.py.
    * New class TestTagProposalsOnProbset in test_problemset.py testing this feature and its settings.

--- oioioi/problems/templatetags/tag.py
+++ oioioi/problems/templatetags/tag.py
@@ -1,21 +1,46 @@
-from django.db.models import prefetch_related_objects
+from django.db.models import Prefetch, prefetch_related_objects
 from django.template import Library
 from django.utils.html import format_html
 from oioioi.base.utils.tags import get_tag_name, get_tag_prefix
+from oioioi.problems.models import AggregatedAlgorithmTagProposal
+from django.utils.translation import gettext as _, ngettext
 
 register = Library()
 
 
 @register.simple_tag
-def prefetch_tags(problems):
-    prefetch_related_objects(
-        problems,
+def prefetch_tags(problems, max_proposals_shown=0, min_proposals_per_tag=1):
+    prefetch_tag_proposals = max_proposals_shown > 0
+
+    lookups = [
         'difficultytag_set',
         'algorithmtag_set__localizations',
         'origintag_set__localizations',
         'origininfovalue_set__localizations',
         'origininfovalue_set__parent_tag__localizations',
-    )
+    ]
+
+    if prefetch_tag_proposals:
+        lookups.append(
+            Prefetch(
+                'aggregatedalgorithmtagproposal_set',
+                queryset=AggregatedAlgorithmTagProposal.objects.filter(
+                    amount__gte=min_proposals_per_tag
+                ).order_by('-amount')[:max_proposals_shown],
+                to_attr='top_tag_proposals'
+            )
+        )
+
+    prefetch_related_objects(problems, *lookups)
+
+    if prefetch_tag_proposals:
+        for problem in problems:
+            algo_tag_pks = set(problem.algorithmtag_set.all().values_list('pk', flat=True))
+            problem.top_tag_proposals = [
+                proposal for proposal in problem.top_tag_proposals
+                if proposal.tag.pk not in algo_tag_pks
+            ]
+
     return u''
 
 
@@ -34,15 +59,22 @@ def tag_label(tag):
 
 @register.simple_tag
 def aggregated_tag_label(aggregated_tag):
-    prefix = get_tag_prefix(aggregated_tag)
+    amount = aggregated_tag.amount
+    tag = aggregated_tag.tag
+
+    full_prefix = get_tag_prefix(aggregated_tag)
+    tag_prefix = get_tag_prefix(tag)
+
+    tag_tooltip = getattr(tag, 'full_name', tag.name)
+    times_text = ngettext("1 time", "%(count)d times", amount) % {'count': amount}
     return format_html(
-        '<a title="{tooltip}" class="badge tag-label tag-label-{cls}" href="{href}" '
-        '>{name} | {amount}</a>',
-        tooltip=getattr(aggregated_tag.tag, 'full_name', aggregated_tag.tag.name),
-        name=get_tag_name(aggregated_tag.tag),
-        cls=prefix,
-        amount=str(aggregated_tag.amount),
-        href="?" + prefix + "=" + aggregated_tag.tag.name,
+        '<a title="{tooltip}" class="badge tag-label tag-label-{cls} position-relative" href="{href}">'
+        '{name}<span class="tag-proposal-amount badge">{amount}</span></a>',
+        tooltip=_(f"{tag_tooltip} — proposed {times_text}"),
+        name=get_tag_name(tag),
+        cls=full_prefix,
+        amount=str(amount),
+        href="?" + tag_prefix + "=" + tag.name,
     )
 
 
