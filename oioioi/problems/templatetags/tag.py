357346f3d0776a96a3544152b08bc0117c107167
Grzegorz Krawczyk <grzes.krawczyk2005@gmail.com>
2025-03-12 17:50:42 +0100

    Change usage of gettext to usage of ngettext.

--- oioioi/problems/templatetags/tag.py
+++ oioioi/problems/templatetags/tag.py
@@ -3,7 +3,7 @@ from django.template import Library
 from django.utils.html import format_html
 from oioioi.base.utils.tags import get_tag_name, get_tag_prefix
 from oioioi.problems.models import AggregatedAlgorithmTagProposal
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext as _, ngettext
 
 register = Library()
 
@@ -66,7 +66,7 @@ def aggregated_tag_label(aggregated_tag):
     tag_prefix = get_tag_prefix(tag)
 
     tag_tooltip = getattr(tag, 'full_name', tag.name)
-    times_text = _("1 time") if amount == 1 else _(f"{amount} times")
+    times_text = ngettext("1 time", "%(count)d times", amount)
     return format_html(
         '<a title="{tooltip}" class="badge tag-label tag-label-{cls} position-relative" href="{href}">'
         '{name}<span class="tag-proposal-amount badge">{amount}</span></a>',
