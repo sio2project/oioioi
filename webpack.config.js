f41d62cda9364bb5ce5493b2451f592d8a6fa450
stopnoanime <birecki@proton.me>
2025-03-15 21:43:20 +0000

    Handle jqtree with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,10 @@ module.exports = {
     jquery: './oioioi/base/static/js/jquery.js',
     index: './oioioi/base/static/js/index.js',
     timeline: './oioioi/timeline/static/timeline/timeline.js',
+    portal_tree: './oioioi/portals/static/portals/portal_tree.js',
   },
   output: {
-    filename: '[name].js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist_webpack'),
   },
   externals: {
