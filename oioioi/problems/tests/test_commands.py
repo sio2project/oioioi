f105e0dafe448b28ea027fa3926dad86c7555303
Jankwi <153385549+Jankwi@users.noreply.github.com>
2026-01-12 09:35:13 +0100

    Expand mass_create_tool's capabilities (#604)
    * Add contest creation to mass_create_tool command
    
    This commit allows for creating and populating (with users & problems) contests,
    using the mass_create_tool management command. Two new arguments are added:
    --createcontest (-cc) to enable contest creation, and --contestname (-cn) to specify the contest's name.
    
    * Add problem package upload option to mass_create_tool
    
    Previously, the mass_create_tool only created empty problems.
    
    This commit changes that by adding a list of problem_packages (-pp)
    as an option to the tool. The tool searches the oioioi/sinolpack/files/ directory
    for the specified problem packages and uploads them to the created contest.
    
    Example command enabled by this commit:
    
        python3 manage.py mass_create_tool -cn=quite_useful_contest -cc -pp=test_full_package.tgz -u=10 --wipe
    
        ^ (This will create a contest named 'quite_useful_contest' with 10 users,
           and upload 'test_full_package.tgz' to a new round in that contest.)
    
    * Add code submissions to mass_create_tool.
    
    Previously the mass_create_tool could only be used for creating uers, problems and contests.
    This commit makes it possible to automatically generate code submissions on behalf of users.

--- oioioi/problems/tests/test_commands.py
+++ oioioi/problems/tests/test_commands.py
@@ -5,6 +5,7 @@ from django.contrib.auth import get_user_model
 from django.core.management import CommandError, call_command
 from django.test import TestCase, override_settings
 
+from oioioi.contests.models import Contest, Submission
 from oioioi.problems.models import (
     AlgorithmTag,
     AlgorithmTagProposal,
@@ -30,19 +31,28 @@ class TestMassCreateTool(TestCase):
         "diffthrough": DifficultyTagThrough,
         "algoproposals": AlgorithmTagProposal,
         "diffproposals": DifficultyTagProposal,
+        "contests": Contest,
+        "submissions": Submission,
     }
 
-    def _assert_model_counts(self, expected_counts):
+    def _assert_model_counts(self, expected_counts, check_problem_names=True):
+        auto_prefix = "_auto_"
         for name, model in self.name_to_model.items():
-            count = model.objects.count()
+            # For submissions, only count those from auto-generated users
+            # (the packages create submissions with model solutions).
+            if name == "submissions":
+                count = model.objects.filter(user__username__startswith=auto_prefix).count()
+            else:
+                count = model.objects.count()
             expected_count = expected_counts.get(name, 0)
             assert count == expected_count, f"Expected {expected_count} {name}, got {count}"
 
-        # Validation for i18n problem names
-        problem_amount: int = expected_counts.get("problems", Problem.objects.count())
-        expected_probname_count = len(settings.LANGUAGES) * problem_amount
-        probname_count = ProblemName.objects.count()
-        assert probname_count == expected_probname_count, f"Expected {expected_probname_count} probnames, got {probname_count}"
+        # Validation for i18n problem names, can be omitted (e.g. for problems for packages).
+        if check_problem_names:
+            problem_amount: int = expected_counts.get("problems", Problem.objects.count())
+            expected_probname_count = len(settings.LANGUAGES) * problem_amount
+            probname_count = ProblemName.objects.count()
+            assert probname_count == expected_probname_count, f"Expected {expected_probname_count} probnames, got {probname_count}"
 
     def test_long_flags(self):
         out = StringIO()
@@ -114,6 +124,48 @@ class TestMassCreateTool(TestCase):
         )
         call_command("mass_create_tool", "--wipe")
         self._assert_model_counts({})
+
+    def test_long_flags_contest(self):
+        out = StringIO()
+        call_command(
+            "mass_create_tool",
+            "--contestname",
+            "demo",
+            "--createcontest",
+            "--problempackages",
+            "test_full_package.tgz",
+            "--users",
+            "10",
+            "--submission_files",
+            "sum-correct.cpp",
+            "sum-various-results.cpp",
+            "--submissions_per_user",
+            "3",
+            "--wipe",
+            stdout=out,
+        )
+        self._assert_model_counts({"users": 10, "problems": 1, "contests": 1, "submissions": 30}, check_problem_names=False)
+
+    def test_short_flags_contest(self):
+        out = StringIO()
+        call_command(
+            "mass_create_tool",
+            "-cn",
+            "demo",
+            "-cc",
+            "-pp",
+            "test_full_package.tgz",
+            "-u",
+            "10",
+            "-sf",
+            "sum-correct.cpp",
+            "sum-various-results.cpp",
+            "-spu",
+            "3",
+            "-w",
+            stdout=out,
+        )
+        self._assert_model_counts({"users": 10, "problems": 1, "contests": 1, "submissions": 30}, check_problem_names=False)
 
     def test_short_flags(self):
         out = StringIO()
