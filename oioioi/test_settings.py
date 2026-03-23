e3467e22e1ff44898e6e16a0e9abb101526ffe75
Michał Burzyński <michal.burzynski0805@gmail.com>
2026-03-23 15:15:53 +0100

    Moved DEFAULT_AUTO_FIELD to oioioi/default_settings.py

--- oioioi/test_settings.py
+++ oioioi/test_settings.py
@@ -9,8 +9,6 @@ SITE_ID = 1
 
 ADMINS = (('Test admin', 'admin@example.com'),)
 
-DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
