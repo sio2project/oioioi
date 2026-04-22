94e5e82ae661c445483384d8dfcadc4916191e36
Olaf Targowski <me@otsrv.net>
2026-04-22 16:53:06 +0200

    fixup for 'Optimize submit view'

--- oioioi/programs/problem_instance_utils.py
+++ oioioi/programs/problem_instance_utils.py
@@ -4,9 +4,7 @@ from django.conf import settings
 def get_allowed_languages_dict(problem_instance):
     if not hasattr(problem_instance, "_cache"):
         lang_dict = getattr(settings, "SUBMITTABLE_EXTENSIONS", {})
-        # TODO: ???
         allowed_langs = problem_instance.problem.controller.get_allowed_languages_for_problem(problem_instance.problem)
-        # TODO: ???
         contest_langs = problem_instance.controller.get_allowed_languages()
         problem_instance._cache = {lang: lang_dict[lang] for lang in lang_dict if lang in allowed_langs and lang in contest_langs}
     return problem_instance._cache
